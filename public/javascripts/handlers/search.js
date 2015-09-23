40670e32b85c84353379091e093266dd378dc991
Ryan McVeety <rpm0618@gmail.com>
2015-09-22 23:37:09 -0400

    Refactor search to work with new endpoint, and infinite scroll

--- public/javascripts/handlers/search.js
+++ public/javascripts/handlers/search.js
@@ -5,11 +5,20 @@ var PAGE_Search = {
 		var elemText = '<li><a href="/story/' + story._id + '">' + story.title + '</a></li>';
 		return $(elemText);
 	},
+	story_offset: 0,
+	story_loading: false,
 	refreshStories: function(callback) {
+		if (PAGE_Search.story_loading) return;
+		PAGE_Search.story_loading = true;
+		
 		$.ajax({
 			url: '/scripts/story/search',
 			type: 'GET',
-			data: {q: PAGE_Search.query},
+			data: {
+				q: PAGE_Search.query,
+				offset: PAGE_Search.story_offset,
+				limit: 10
+			},
 			success: function(result) {
 				if (Object.keys(result.err).length > 0) {
 					$.snackbar({content: resolveError(result.err)});
@@ -18,33 +27,47 @@ var PAGE_Search = {
 				result.data.forEach(function(story){
 					var elem = PAGE_Search.makeStoryListItem(story);
 					$('#stories').append(elem);
+					PAGE_Search.story_offset += 1;
 				});
 			},
 			error: function(xhr, status, error ){
 				callback(error, null);
+			},
+			complete: function(){
+				PAGE_Search.story_loading = false;
 			}
 		});
 	},
+	post_offset: 0,
+	post_loading: false,
 	refreshPosts: function(callback) {
+		if (PAGE_Search.post_loading) return;
+		PAGE_Search.post_loading = true;
+		
 		$.ajax({
 			url: '/scripts/gallery/search',
 			type: 'GET',
-			data: {q: PAGE_Search.query},
+			data: {
+				q: PAGE_Search.query,
+				offset: PAGE_Search.post_offset,
+				limit: 12
+			},
 			success: function(result) {
 				if (Object.keys(result.err).length > 0) {
 					$.snackbar({content: resolveError(result.err)});
 					return callback(result.err, null);
 				}
-				result.data.forEach(function(gallery){
-					gallery.posts.forEach(function(post){
-						var elem = buildPost(post, purchases ? purchases.indexOf(post._id) != -1 : null, 'large', true);
-						$('#posts').append(elem);
-					});
+				result.data.forEach(function(post){
+					var elem = buildPost(post, purchases ? purchases.indexOf(post._id) != -1 : null, 'large', true);
+					$('#posts').append(elem);
+					PAGE_Search.post_offset += 1;
 				});
-				console.log(result);
 			},
 			error: function(xhr, status, error ){
 				callback(error, null);
+			},
+			complete: function(){
+				PAGE_Search.post_loading = false;
 			}
 		});
 	},
@@ -56,4 +79,9 @@ var PAGE_Search = {
 
 $(document).ready(function() {
 	PAGE_Search.refresh();
+	
+	$('.container-fluid.grid').scroll(function() {
+		if(!PAGE_Search.post_loading && $('.container-fluid.grid')[0].scrollHeight - $('.container-fluid.grid').scrollTop() <= $('.container-fluid.grid').height() + 64)
+			PAGE_Search.refreshPosts();
+	});
 });
