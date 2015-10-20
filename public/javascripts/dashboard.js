7a3bf15f6f76541d7789209870e4d87d957a9f36
Ryan McVeety <rpm0618@gmail.com>
2015-10-19 22:31:06 -0400

    More complete gallery create functionality

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -311,6 +311,116 @@ $(document).ready(function(){
 		}
 	});
 
+	$('#gallery-create-stories-input').typeahead({
+		hint: true,
+		highlight: true,
+		minLength: 1,
+		classNames: {
+			menu: 'tt-menu shadow-z-2'
+		}
+	},
+	{
+		name: 'stories',
+		display: 'title',
+		source: function(query, syncResults, asyncResults){
+			$.ajax({
+				url: '/scripts/story/search',
+				data: {
+					q: query
+				},
+				success: function(result, status, xhr){
+					asyncResults(result.data || []);
+				},
+				error: function(xhr, statur, error){
+					console.log(error);
+					asyncResults([]);
+				}
+			});
+		},
+		templates: {
+			empty: [
+				'<div class="story-empty-message tt-suggestion">',
+					'Create new story',
+				'</div>'
+			].join('\n'),
+		}
+	}).on('typeahead:select', function(ev, story){
+		var elem = makeTag(story.title);
+		elem.data('id', story._id);
+		if($('#gallery-create-stories-list li.chip').map(function(elem){return $(this).data('id')}).toArray().indexOf(story._id) == -1)
+			$('#gallery-create-stories-list').append(elem);
+		else
+			$.snackbar({content: 'This gallery is already in that story!'});
+			
+		$(this).typeahead('val', '');
+	}).on('keydown', function(ev){
+		if (ev.keyCode == 13 && $('.story-empty-message').length == 1) {
+			if($(this).val().indexOf('http://') != -1) {
+				$.snackbar({content: 'No URLs please!'});
+				return;
+			}
+			var elem = makeTag($(this).val());
+			var id = 'NEW={"title":"' + $(this).val() + '"}';
+			elem.data('id', id);
+			console.log(elem.data('id'));
+			elem.addClass('new-story');
+			if($('#gallery-create-stories-list li.chip').map(function(elem){return $(this).data('id')}).toArray().indexOf(id) == -1)
+				$('#gallery-create-stories-list').append(elem);
+			else
+				$.snackbar({content: 'This gallery is already in that story!'});
+			$(this).typeahead('val', '');
+		}
+	});
+
+	$('#gallery-create-articles-input').typeahead({
+		hint: true,
+		highlight: true,
+		minLength: 1,
+		classNames: {
+			menu: 'tt-menu shadow-z-2',
+			suggestion: 'article-suggestion'
+		}
+	},
+	{
+		name: 'articles',
+		display: 'link',
+		source: function(query, syncResults, asyncResults){
+			$.ajax({
+				url: '/scripts/article/search',
+				data: {
+					q: query
+				},
+				success: function(result, status, xhr){
+					asyncResults(result.data || []);
+				},
+				error: function(xhr, statur, error){
+					console.log(error);
+					asyncResults([]);
+				}
+			});
+		},
+		templates: {
+			empty: [
+				'<div class="article-empty-message tt-suggestion">',
+					'Create new article',
+				'</div>'
+			].join('\n'),
+		}
+	}).on('typeahead:select', function(ev, article){
+		var elem = makeTag(article.link);
+		elem.data('id', article._id);
+		$('#gallery-create-articles-list').append(elem);
+		$(this).typeahead('val', '');
+	}).on('keydown', function(ev){
+		if (ev.keyCode == 13 && $('.article-empty-message').length == 1) {
+			var elem = makeTag($(this).val());
+			elem.data('id', 'NEW={"link":"' + $(this).val() + '"}');
+			elem.addClass('new-story');
+			$('#gallery-create-articles-list').append(elem);
+			$(this).typeahead('val', '');
+		}
+	});
+
 	//Clear bulk selection
 	$('#bulk-clear-button').click(function(){
 		$('.tile.toggled').each(function(i, elem){
@@ -374,12 +484,14 @@ var GALLERY_EDIT = null,
 	edit_marker = null;
 
 //AJAX - Create new gallery
-function createGallery(caption, tags, posts, highlight, callback){
+function createGallery(caption, tags, posts, highlight, articles, stories, callback){
 	var params = {
 		caption: caption,
 		posts: posts,
 		tags: tags,
-		highlight: highlight,
+		visibility: highlight ? 2 : 1,
+		articles: articles,
+		stories: stories,
 	};
 	$.ajax("/scripts/gallery/create", {
 		method: 'post',
@@ -723,9 +835,13 @@ function buildPost(post, purchased, size, forsale){
 
 	attachOnImageLoadError(elem.find('img.img-cover'));
 	elem.find('img.img-cover').prop('src',formatImg(post.image, 'small'));
-	elem.find('.tile-body').click(function(){ window.location.assign('/post/' + post._id)}).css('cursor', 'pointer');
+	elem.find('.tile-body').click(function(event){
+		if(event.shiftKey) return;
+		window.location.assign('/post/' + post._id)
+	}).css('cursor', 'pointer');
 	elem.click(function(event) {
-		if (event.shiftKey && !$(this).hasClass("story") && $(this).find('.mdi-library-plus').length > 0) {
+		//if (event.shiftKey && !$(this).hasClass("story") && $(this).find('.mdi-library-plus').length > 0) {
+		if (event.shiftKey && !$(this).hasClass("story")) {
 			toggleSelected(this, post);
 		}
 	});
@@ -1088,12 +1204,14 @@ function galleryCreateClear(){
 //Create the new gallery
 function galleryCreateSave(){
 	var caption = $('#gallery-create-caption-input').val();
-	var tags = $('.tag').text().split('#').filter(function(t){ return t.length > 0; });
+	var tags = $('#gallery-create-tags-list .tag').text().split('#').filter(function(t){ return t.length > 0; });
+	var articles = $('#gallery-create-articles-list li.chip').map(function(elem){return $(this).data('id')}).toArray();
+	var stories = $('#gallery-create-stories-list li.chip').map(function(elem){return $(this).data('id')}).toArray();
 	var posts = selectedPosts.map(function(post){ return post._id });
 
 	var highlight = $('#gallery-create-highlight-input').prop('checked');
 
-	createGallery(caption, tags, posts, highlight, function(err, gallery){
+	createGallery(caption, tags, posts, highlight, articles, stories, function(err, gallery){
 		if (err)
 			$.snackbar({content: resolveError(err)});
 		else
