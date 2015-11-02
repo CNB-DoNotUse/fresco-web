132ca78027f900f23e07e050bd7aebfbed8df8d6
Mike <michaelterpak@gmail.com>
2015-11-02 17:58:18 -0500

    pulling

--- public/javascripts/handlers/outlet-settings.js
+++ public/javascripts/handlers/outlet-settings.js
@@ -1,70 +1,43 @@
 var PAGE_OutletSettings = {
-	saving: false,
+	
+};
 
-
-
-	setDisabled: function(disabled){
-		$('.outlet-field').prop('disabled', disabled);
-	},
-
-	saveCard: function(params, callback){
-		if (PAGE_OutletSettings.saving)
-			return false;
-		if (params.number == '' || params.cvv == '' || params['exp-month'] == '' || params['exp-year'] == '')
-			return $.snackbar({content:'All fields must be filled'});
-
-		PAGE_OutletSettings.saving = true;
-		PAGE_OutletSettings.setDisabled(true);
-
-		var	params = {
-				number: $('.stripe-ccn').val(),
-				cvv: $('.stripe-cvv').val(),
-				'exp-month': $('.stripe-month').val(),
-				'exp-year': $('.stripe-year').val(),
-				'currency': 'usd'
+$(document).ready(function(){
+	//Save card info
+	$('.card-outlet-card > .outlet-card-save').on('click', function(){
+		var _this = $(this),
+			exp_times = $('.card-outlet-card > .payment-content .payment-exp').val().split('/'),
+			params = {
+				"number": $('.card-outlet-card > .payment-content .payment-ccn').val(),
+				"cvv": $('.card-outlet-card > .payment-content .payment-cvv').val(),
+				"exp-month": exp_times[0].trim(),
+				"exp-year": (exp_times[1] || '').trim(),
+				"address_zip": $('.card-outlet-card > .payment-content .payment-zip').val(),
+				"name": $('.card-outlet-card > .payment-content .payment-name').val(),
+				"currency": "usd",
 			};
-
-		$('stripe-field').removeClass('error');
-
-		if (!Stripe.card.validateCardNumber(params.number)){
-			$('.stripe-ccn').addClass('error');
-			PAGE_OutletSettings.saving = false;
-			PAGE_OutletSettings.setDisabled(false);
+			
+		if (!Stripe.card.validateCardNumber(params.number))
 			return $.snackbar({content:'Invalid credit card number'});
-		}
-		if (!Stripe.card.validateExpiry(params['exp-month'], params['exp-year'])){
-			$('.stripe-month').addClass('error');
-			$('.stripe-year').addClass('error');
-			PAGE_OutletSettings.saving = false;
-			PAGE_OutletSettings.setDisabled(false);
+		if (!Stripe.card.validateExpiry(params['exp-month'], params['exp-year']))
 			return $.snackbar({content:'Invalid expiration date'});
-		}
-		if (!Stripe.card.validateCVC(params.cvv)){
-			$('.stripe-cvv').addClass('error');
-			PAGE_OutletSettings.saving = false;
-			PAGE_OutletSettings.setDisabled(false);
+		if (!Stripe.card.validateCVC(params.cvv))
 			return $.snackbar({content:'Invalid CVV number'});
-		}
-
+			
+		_this.prop('disabled', true);
+		
 		var stripeResponseHandler = function(status, response) {
 			if (response.error){
-				PAGE_OutletSettings.saving = false;
-				PAGE_OutletSettings.setDisabled(false);
+				_this.prop('disabled', false);
 				return $.snackbar({content: response.error.message});
 			}
-
-			var saveparams = new FormData();
-			saveparams.append('id', PAGE_OutletSettings.outlet._id);
-			saveparams.append('stripe_token', response.id);
 
 			$.ajax({
 				url: "/scripts/outlet/update",
 				type: 'POST',
-				data: saveparams,
-				dataType: 'json',
-				cache:false,
-				contentType:false,
-				processData:false,
+				data: {
+					stripe_token: response.id
+				},
 				success: function(result, status, xhr){
 					if (result.err)
 						return this.error(null, null, result.err);
@@ -73,43 +46,41 @@ var PAGE_OutletSettings = {
 					window.location.reload();
 				},
 				error: function(xhr, status, error){
-					PAGE_OutletSettings.saving = false;
-					PAGE_OutletSettings.setDisabled(false);
 					$.snackbar({content:resolveError(error)});
+				},
+				complete: function(){
+					_this.prop('disabled', false);
 				}
 			});
 		};
-
+		
 		var form = $('<form></form>');
 
 		for (var index in params)
 			form.append('<input type="hidden" data-stripe="' + index + '" value="' + params[index] + '">');
 
 		Stripe.card.createToken(form, stripeResponseHandler);
-	},
-
-	saveProfile: function(){
-		if (PAGE_OutletSettings.saving)
-			return false;
-
-		var params = new FormData(),
-			files = $('.outlet-edit-avatar');
-
-		params.append('id', PAGE_OutletSettings.outlet._id);
-		params.append('title', $('.outlet-edit-title').val());
-		params.append('link', $('.outlet-edit-link').val());
-
-		if (files.prop('files')[0])
-			params.append('avatar', files.prop('files')[0]);
-
+	});
+	
+	//Save outlet info
+	$('.card-outlet-info > .card-controls > .outlet-info-save').on('click', function(){
+		var _this = $(this),
+			avatarFiles = $('.card-outlet-info > .outlet-avatar').prop('files'),
+			params = new FormData();
+			
+		if (avatarFiles && avatarFiles.length > 0) params.append('avatar', avatarFiles[0]);
+		params.append('bio', $('.card-outlet-info > .card-controls > .outlet-bio').val());
+		params.append('link', $('.card-outlet-info > .card-controls > .outlet-website').val());
+		params.append('title', $('.card-outlet-info > .card-controls > .outlet-name').val());
+			
 		$.ajax({
 			url: "/scripts/outlet/update",
 			type: 'POST',
 			data: params,
 			dataType: 'json',
-					cache:false,
-					contentType:false,
-					processData:false,
+			cache:false,
+			contentType:false,
+			processData:false,
 			success: function(result, status, xhr){
 				if (result.err)
 					return this.error(null, null, result.err);
@@ -118,26 +89,106 @@ var PAGE_OutletSettings = {
 				window.location.reload();
 			},
 			error: function(xhr, status, error){
-				PAGE_OutletSettings.saving = false;
-				PAGE_OutletSettings.setDisabled(false);
-				$.snackbar({content: resolveError(error)});
+				$.snackbar({content:resolveError(error)});
+			},
+			complete: function(){
+				_this.prop('disabled', false);
 			}
 		});
-	},
+	});
+	
+	//Avatar controls
+	$('.card-outlet-info > .card-image > .img-overlay').on('click', function(){
+		$('.card-outlet-info > .outlet-avatar').trigger('click');
+	});
+	$('.card-outlet-info > .outlet-avatar').on('change', function(e){
+		var _this = this;
+		
+		if (_this.files && _this.files[0]) {
+			var reader = new FileReader();
+	
+			reader.onload = function (e) {
+				$('.card-outlet-info > .card-image > .img').css('background-image', 'url("'+e.target.result+'")');
+			}
+	
+			reader.readAsDataURL(_this.files[0]);
+		}
+	});
+	
+	//Disable all disabled checkboxes
+	$('.checkbox.disabled').each(function(i, v){
+		$(v).find('label > input').prop('disabled', true);
+	});
+	
+	$('.card-outlet-members .notify-email').change(function(){
+		if ($('.card-outlet-members .notify-email-all').is(':checked') || $('.card-outlet-members .notify-email:not([disabled]):not(:checked)').length === 0)
+			$('.card-outlet-members .notify-email-all').click();
+		else if ($('.card-outlet-members .notify-email:not([disabled]):not(:checked)').length === 0)
+			$('.card-outlet-members .notify-email-all').click();
+	});
+	$('.card-outlet-members .notify-sms').change(function(){
+		if ($('.card-outlet-members .notify-sms-all').is(':checked'))
+			$('.card-outlet-members .notify-sms-all').click();
+		else if ($('.card-outlet-members .notify-sms:not([disabled]):not(:checked)').length === 0)
+			$('.card-outlet-members .notify-sms-all').click();
+	});
+	$('.card-outlet-members .notify-email-all').change(function(){
+		if ($(this).is(':checked'))
+			$('.card-outlet-members .notify-email:not(:checked)').click();
+		else if ($('.card-outlet-members .notify-email:not([disabled]):not(:checked)').length === 0)
+			$('.card-outlet-members .notify-email:checked').click();
+	});
+	$('.card-outlet-members .notify-sms-all').change(function(){
+		if ($(this).is(':checked'))
+			$('.card-outlet-members .notify-sms:not(:checked)').click();
+		else if ($('.card-outlet-members .notify-sms:not([disabled]):not(:checked)').length === 0)
+			$('.card-outlet-members .notify-sms:checked').click();
+	});
+		
+	if ($('.card-outlet-members .notify-sms:not([disabled]):not(:checked)').length === 0)
+		$('.card-outlet-members .notify-sms-all').click();
+	if ($('.card-outlet-members .notify-sms:not([disabled]):not(:checked)').length === 0)
+		$('.card-outlet-members .notify-sms-all').click();
+		
+	$('.outlet-members > li > .delete-member').on('click', function(){
+		var _this = $(this),
+			listItem = _this.parent('li');
+		
+		_this.prop('disabled', true);
 
-	invite: function(e){
-		var textarea = $('textarea.outlet-invite'),
-			button = $('button.outlet-invite');
+		$.ajax({
+			url: "/scripts/outlet/user/remove",
+			method: 'post',
+			data: {
+				user: listItem.data('user-id')
+			},
+			success: function(result, status, xhr){
+				if (result.err)
+					return this.error(null, null, result.err);
+					
+				listItem.slideUp("fast", function() { $(this).remove(); } );
+			},
+			error: function(xhr, status, error){
+				$.snackbar({content: resolveError(error)});
+			},
+			complete: function(){
+				_this.prop('disabled', false);
+			}
+		});
+	});
+	
+	$('input.outlet-invite').on('keypress', function(e){
+		if (e.which !== 13) return;
+		
+		var _this = $(this);
 
-		textarea.prop('disabled', true);
-		button.prop('disabled', true);
+		_this.prop('disabled', true);
 
-		var addresses = textarea.val().split(' ');
+		var addresses = _this.val().split(' ');
 
 		if (addresses == '' || (addresses.length == 1 && addresses[0].split() == '')){
 			e.preventDefault();
-			textarea.prop('disabled', false);
-			button.prop('disabled', false);
+			_this.prop('disabled', false);
 			$.snackbar({content:'Must invite at least 1 member'});
 			return false;
 		}
@@ -154,55 +205,15 @@ var PAGE_OutletSettings = {
 				if (result.err)
 					return this.error(null, null, result.err);
 
-				textarea.val('');
+				_this.val('');
 				$.snackbar({content: (addresses.length == 1 ? 'Invitation' : 'Invitations') + ' successfully sent!'});
 			},
 			error: function(xhr, status, error){
 				$.snackbar({content: resolveError(error)});
 			},
 			complete: function(){
-				textarea.prop('disabled', false);
-				button.prop('disabled', false);
-			}
-		});
-	}
-};
-
-$(document).ready(function(){
-	$('.outlet-field').filter(function(){return this.value != '';}).trigger('keydown');
-	$('.outlet-field').filter(function(){return this.value == '';}).trigger('keyup');
-
-	$('.outlet-remove-user').on('click', function(evt){
-		var listitem = $(this).parent().parent();
-
-		$.ajax({
-			url: "/scripts/outlet/user/remove",
-			method: 'post',
-			contentType: "application/json",
-			data: JSON.stringify({
-				user: $(this).data('uid')
-			}),
-			dataType: 'json',
-			success: function(result, status, xhr){
-				if (result.err)
-					return this.error(null, null, result.err);
-				listitem.slideUp("fast", function() { $(this).remove(); } );
-			},
-			error: function(xhr, status, error){
-				$.snackbar({content: resolveError(error)});
+				_this.prop('disabled', false);
 			}
 		});
 	});
-
-	$('textarea.outlet-invite').keypress(function(e){
-		if (e.which == 13){
-			PAGE_OutletSettings.invite(e);
-		}
-	});
-	$('button.outlet-invite').click(function(e){
-		PAGE_OutletSettings.invite(e);
-	});
-
-	$('.profile-save').on('click', PAGE_OutletSettings.saveProfile);
-	$('.stripe-save').on('click', PAGE_OutletSettings.saveCard);
 });
