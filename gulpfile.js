e49656a5e68166639551cc125e63281710d7f982
Nolan Leung <nolan@fresconews.com>
2015-12-10 16:49:50 -0500

    Implemented Dependency Management

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,91 @@
-var fs = require('fs'),
-	gulp = require('gulp'),
-	gutil = require("gulp-util"),
-	webpack = require("webpack-stream"),
-	sass = require('gulp-sass');
+var fs 				= require('fs'),
+	gulp 			= require('gulp'),
+	webpack 		= require('webpack-stream'),
+	merge 			= require('merge-stream');
 
-var exclude = ['app.js'];
+var sass 			= require('gulp-sass'),
+	concat 			= require('gulp-concat'),
+	minifyCss		= require('gulp-minify-css'),
+	uglify			= require('gulp-uglify'),
+	source			= require('vinyl-source-stream'),
+	colors 			= require('colors');
 
-var views = {};
 
-var files = fs.readdirSync('./app/views');
-files.map(function(file) {
-  if(exclude.indexOf(file) != -1 || !/.jsx?$/.test(file)) 
-    return;
-  views[file.replace('.js', '')] = './app/views/' + file;
+var dependencies 	= require('./lib/dependencies'),
+	exclude 		= ['app.js', 'extra.js'];
+
+var sections			= Object.keys(dependencies),
+	pages 				= {};
+
+	sections.splice(sections.indexOf('global'), 1);
+
+var views 			= {},
+	viewFiles 		= fs.readdirSync('./app/views');
+
+viewFiles.map((file) => {
+  if(exclude.indexOf(file) != -1 || !/.jsx?$/.test(file)) return;
+  views[file.replace('.js', '')] = ['./app/views/' + file];
 });
 
-gulp.task('css', function () {
-	gulp.src('./app/sass/screen.scss')
-		.pipe(sass().on('error', sass.logError))
-		.pipe(gulp.dest('./public/stylesheets'));
+gulp.task('Build',  () => {
+
+	var cssTasks = [], jsTasks = [];
+
+	// Build page specific CSS and JS
+	for (var s in sections) {
+		//Define the sections we're in
+		var section = sections[s];
+
+		//Define all the pages in this sections
+		var pages = Object.keys(dependencies[section]);
+
+		// Build section _global css
+		cssTasks.push(
+			gulp.src(dependencies.global.css.concat(dependencies[section]._global.css))
+				.pipe(concat(section + '.css'))
+				.pipe(sass().on('error', sass.logError))
+				.pipe(minifyCss())
+				.pipe(gulp.dest('./public/stylesheets'))
+		);
+
+		// Build section _global JS
+		jsTasks.push(
+			gulp.src(dependencies.global.js.concat(dependencies[section]._global.js))
+				.pipe(concat(section + '.js'))
+				.pipe(uglify())
+				.pipe(gulp.dest('./public/javascripts'))
+		);
+
+		//Take out the global from the pages
+		//so now we onyl build the page specific stuff
+		pages.splice(pages.indexOf('_global'), 1);
+		
+		//Loop through all the pages in the sections, not part of the `_global`
+		for(var p in pages) {
+			
+			var pageDependencies = dependencies[section][pages[p]];
+
+			cssTasks.push(
+				gulp.src(dependencies.global.css.concat(pageDependencies.css))
+					.pipe(concat(pages[s] + '.css'))
+					.pipe(sass().on('error', sass.logError))
+					.pipe(minifyCss())
+					.pipe(gulp.dest('./public/stylesheets/page'))
+			);
+
+			jsTasks.push(
+				gulp.src(dependencies.global.js.concat(pageDependencies.js))
+					.pipe(concat(pages[s] + '.js'))
+					.pipe(uglify())
+					.pipe(gulp.dest('./public/javascripts/page'))
+			);
+		}
+	}
+
+	return merge(cssTasks, jsTasks);
 });
 
-gulp.task('js', function(cb) {
+gulp.task('js', (cb) => {
 	return gulp.src('app/views/app.js')
 		.pipe(webpack({
 			watch: true,
@@ -45,8 +109,8 @@ gulp.task('js', function(cb) {
 		.pipe(gulp.dest('./public/javascripts/pages'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
 	gulp.watch('./app/sass/**/*.scss', ['css']);
 });
 
-gulp.task('default', ['watch', 'css', 'js']);
+gulp.task('default', ['Build']);
