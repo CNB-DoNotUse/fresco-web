cfdd346414485352e2c79357157d9d9283b13816
Elmir Kouliev <elmir@kouliev.com>
2015-12-30 12:02:21 -0500

    Fixed scroll bug on index page

--- public/js/handlers/index.js
+++ public/js/handlers/index.js
@@ -36,7 +36,9 @@ var ticking = false,
 	nav = document.getElementById('_nav'),
 	lastScrollY = 0,
 	navReached = false,
-	translate3dSupported = animation.has3d();
+	translate3dSupported = animation.has3d(),
+	initialDiff = $(hero).offset().top + hero.clientHeight - $(bottom).offset().top;
+
 
 window.addEventListener('scroll', function(e) {
 
@@ -46,37 +48,28 @@ window.addEventListener('scroll', function(e) {
 	//Check if translate3dSupported is suppored or Check if not animating 
 	if(!translate3dSupported || ticking) return;
 
-	var diff = $(hero).offset().top + hero.clientHeight - $(bottom).offset().top;
-
-	//Check if maximum space difference 
-	//between the hero and the cards is reached
-	if(diff > 0){ 
-
-		//Check direciton of scroll
-		if(!(lastScrollY > window.pageYOffset))
-			return;
-
-	}
 
 	window.requestAnimFrame(function(){
 
-		lastScrollY = window.pageYOffset;
+		scrollY = window.pageYOffset;
 
-		var heroValue = -lastScrollY / 6 < 0 ? -lastScrollY / 6 : 0,
-			bottomValue = -lastScrollY / .3 < 0 ? -lastScrollY / .3 : 0,
-			navValue = -lastScrollY / .5 < 0 ? lastScrollY / .5 : 0;
-
-		console.log('Expected Position', diff + -bottomValue);
+		var heroValue = -scrollY / 6 < 0 ? -scrollY / 6 : 0,
+			bottomValue = -scrollY / .3 < 0 ? -scrollY / .3 : 0,
+			navValue = -scrollY / .5 < 0 ? scrollY / .5 : 0;
 
 		//Make sure our nav bar stops, and remains at 96px after showing
 		if(navValue >= 96 || navReached) {
 			navValue = 96;
 			navReached = true;
 		}
-		
+
+		animation.translateY3d(nav, navValue);
+
+		if(bottomValue < initialDiff)
+			bottomValue = initialDiff;
+
 		animation.translateY3d(hero,   heroValue);
 		animation.translateY3d(bottom, bottomValue);
-		animation.translateY3d(nav,    navValue);
 
 
 		ticking = false;
