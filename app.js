0e436104ed8dcd5bfbf66053943a54d3c71402f6
Nolan Leung <nolan@fresconews.com>
2016-01-14 16:49:38 -0500

    Add compression to node

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var config        = require('./lib/config'),
     global        = require('./lib/global'),
     routes        = require('./lib/routes'),
     express       = require('express'),
+    compression   = require('compression'),
     path          = require('path'),
     favicon       = require('serve-favicon'),
     morgan        = require('morgan'),
@@ -13,7 +14,7 @@ var config        = require('./lib/config'),
     bodyParser    = require('body-parser'),
     multer        = require('multer'),
     fs            = require('fs'),
-    http	  = require('http'),
+    http	       =  require('http'),
     https         = require('https'),
     requestJson   = require('request-json'),
     request       = require('superagent');
@@ -32,6 +33,7 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // app.use(morgan('dev'));
 
+app.use(compression())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(multer({
