ea88ebae89a4eea3d9d10defa9b1926005130215
Ryan McVeety <rpm0618@gmail.com>
2016-03-01 22:42:24 -0500

    Update build system

--- docker/docker.js
+++ docker/docker.js
@@ -1,63 +1,77 @@
 'use strict'
 
 const yargs = require('yargs');
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
+const child_process = require('child_process');
+
+function buildDocker(buildConfig) {
+    copyFile(normalizePath('.dockerignore'), normalizePath('../.dockerignore'));
+    copyFile(normalizePath(`${buildConfig}/Dockerfile`), normalizePath('../Dockerfile'));
+    copyFile(normalizePath(`${buildConfig}/docker-compose.yml`), normalizePath('../docker-compose.yml'));
+    copyFile(normalizePath(`${buildConfig}/docker.json`), normalizePath('../config/docker.json'));
+
+    runCommand('docker build -t fresco-web .');
+}
+
+function clean() {
+    deleteFile(normalizePath('../.dockerignore'));
+    deleteFile(normalizePath('../Dockerfile'));
+    deleteFile(normalizePath('../docker-compose.yml'));
+    deleteFile(normalizePath('../config/docker.json'));
+}
+
+function normalizePath(filePath) {
+    return path.join(__dirname, filePath);
+}
+
+function copyFile(from, to) {
+    console.log(`Copying ${from} to ${to}`);
+    fs.writeFileSync(to, fs.readFileSync(from));
+}
+
+function deleteFile(file) {
+    if (fileExists(file)) {
+        console.log(`Deleting ${file}`);
+        fs.unlinkSync(file);
+    }
+    else {
+        console.log(`${file} doesn't exist. Skipping`)
+    }
+}
+
+function fileExists(file) {
+    try {
+        fs.accessSync(file);
+        return true;
+    }
+    catch (ex) {
+        return false;
+    }
+}
+
+function runCommand(command) {
+    console.log(`Running ${command}`)
+    child_process.execSync(command, {
+        cwd: normalizePath('../'),
+        stdio: 'inherit'
+    });
+}
 
 function main() {
     let argv = yargs.argv;
 
-    if (argv._.indexOf('nobuild') !== -1) {
-        copyFiles('nobuild');
+    let buildConfig = argv._[0];
+
+    if (!buildConfig) {
+        console.log("Please specify a build configuration");
     }
-    else if (argv._.indexOf('clean') !== -1) {
+    else if (buildConfig == 'clean') {
         clean();
     }
     else {
-        copyFiles('dobuild');
+        buildDocker(buildConfig);
     }
-}
-
-function copyFiles(type) {
-    let srcDockerPath = path.join(__dirname, type);
-    let dstDockerPath = path.join(__dirname, '../');
-
-    console.log(`Copying docker files from ${srcDockerPath} to ${dstDockerPath}`);
-    copy(path.join(srcDockerPath, 'Dockerfile'), path.join(dstDockerPath, 'Dockerfile'));
-    copy(path.join(srcDockerPath, 'docker-compose.yml'), path.join(dstDockerPath, 'docker-compose.yml'));
-
-    let dockerConfig = {
-        "REDIS": {
-          "SESSIONS": "redis"
-        }
-    }
-
-    let dockerConfigContents = JSON.stringify(dockerConfig, null, 4);
-
-    let dockerConfigFile = path.join(__dirname, '../config/docker.json');
-
-    console.log(`Writing config:\n${dockerConfigContents}\nto ${dockerConfigFile}`)
-
-    fs.writeFileSync(dockerConfigFile, dockerConfigContents);
-}
-
-function copy(from, to) {
-    fs.createReadStream(from).pipe(fs.createWriteStream(to));
-}
-
-function clean() {
-    let dockerfile = path.join(__dirname, '../Dockerfile');
-    let dockerComposeFile = path.join(__dirname, '../docker-compose.yml');
-    let dockerConfigFile = path.join(__dirname, '../config/docker.json');
-
-    console.log(`Removing ${dockerfile}`);
-    fs.unlinkSync(dockerfile);
-
-    console.log(`Removing ${dockerComposeFile}`);
-    fs.unlinkSync(dockerComposeFile);
-
-    console.log(`Removing ${dockerConfigFile}`);
-    fs.unlinkSync(dockerConfigFile);
 }
 
 main();
