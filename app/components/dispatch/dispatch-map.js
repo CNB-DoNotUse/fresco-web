385c3c1c7a8aefc5ab20e3e69b30a6db4d38ce86
Ryan McVeety <rpm0618@gmail.com>
2016-03-21 19:32:36 -0400

    Revert "Merge pull request #9 from fresconews/dev-smoothzoom/WEB-18"
    This reverts commit 5a5dae9f3360efd72ab71d4869404cff1c57cd20, reversing
    changes made to c109690cb6e70ba9ee4f3e5b043e5de2b368c4d7.

--- app/components/dispatch/dispatch-map.js
+++ app/components/dispatch/dispatch-map.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/server'
 import DispatchMapCallout from './dispatch-map-callout'
 import _ from 'lodash'
-import global from '../../../lib/global'
+import global from '../../../lib/global' 
 
 /**
 * Description : The container for the map element in the dispatch page
@@ -41,8 +41,6 @@ export default class DispatchMap extends React.Component {
 		this.addAssignmentToMap = this.addAssignmentToMap.bind(this);
 		this.addAssignmentMarker = this.addAssignmentMarker.bind(this);
 		this.saveMapLocation = this.saveMapLocation.bind(this);
-		this.smoothPan = this.smoothPan.bind(this);
-		this.smoothZoom = this.smoothZoom.bind(this);
 	}
 
 	componentDidMount() {
@@ -54,12 +52,12 @@ export default class DispatchMap extends React.Component {
 				mapZoom: 12
 			});
 		}
-
+		
 		var dispatch = JSON.parse(window.sessionStorage.dispatch);
-
+		
 		//Set up the map object
 		var map = new google.maps.Map(
-			document.getElementById('map-canvas'),
+			document.getElementById('map-canvas'), 
 			{
 				zoom: dispatch.mapZoom,
 				center: dispatch.mapCenter,
@@ -78,16 +76,16 @@ export default class DispatchMap extends React.Component {
 			this.updateMap();
 		}, 2000);
 
-		this.setState({ map: map });
+		this.setState({ map: map });	
 	}
 
 	componentDidUpdate(prevProps, prevState) {
 
 		//Check if there is an active assignment or the acive assignment has `changed`
-		if(!this.isOpeningCallout &&
-			this.props.activeAssignment &&
-			(!prevProps.activeAssignment || prevProps.activeAssignment._id != this.props.activeAssignment._id))
-		{
+		if(!this.isOpeningCallout && 
+			this.props.activeAssignment && 
+			(!prevProps.activeAssignment || prevProps.activeAssignment._id != this.props.activeAssignment._id)) 
+		{	
 			this.focusOnAssignment(this.props.activeAssignment);
 		}
 
@@ -133,7 +131,7 @@ export default class DispatchMap extends React.Component {
 			});
 		}
 
-		//Check if there's already a new assignment marker
+		//Check if there's already a new assignment marker 
 		//Meaning the marker has already been added, and we don't need to add a new one
 		if(this.state.newAssignmentMarker && this.state.newAssignmentCircle){
 
@@ -152,13 +150,13 @@ export default class DispatchMap extends React.Component {
 			if(this.props.newAssignment){
 				//Compare to make sure we don't change the marker unless its position hasn't actually changed
 				if(JSON.stringify(this.props.newAssignment.location) !== JSON.stringify(prevMarkerLoc)){
-					marker.setPosition(this.props.newAssignment.location);
-					map.setCenter(marker.getPosition());
+					marker.setPosition(this.props.newAssignment.location);	
+					map.setCenter(marker.getPosition());	     
 				}
 
 				//Check if circle center has changed
 				if(JSON.stringify(this.props.newAssignment.location) !== JSON.stringify(prevCircleLoc)){
-					circle.setCenter(marker.getPosition());
+					circle.setCenter(marker.getPosition());	     
 				}
 
 				//Check if circle radius has changed
@@ -188,21 +186,21 @@ export default class DispatchMap extends React.Component {
 				location = {
 					lat: marker.getPosition().lat(),
 					lng: marker.getPosition().lng()
-				};
+				};	
 
 			//Update the position to the parent component
 			this.props.updateNewAssignment(location, null, null);
 
 			//Set marker to state of map so we can manage its location
-			this.setState({
+			this.setState({ 
 				newAssignmentMarker: marker,
-				newAssignmentCircle: circle
+				newAssignmentCircle: circle 
 			});
 
 			//Update the maps center to reflect the new positon of the marker
 			this.state.map.setCenter(
 				marker.getPosition()
-			);
+			);	     
 
 			google.maps.event.addListener(marker, 'drag', (ev) => {
 				//Send up location to the parent
@@ -210,11 +208,11 @@ export default class DispatchMap extends React.Component {
 					{
 						lat: ev.latLng.lat(),
 						lng: ev.latLng.lng()
-					},
+					}, 
 					this.props.newAssignment.radius,
 					this.state.map.getZoom(),
 					'markerDrag'
-				);
+				);	
 			});
 		}
 	}
@@ -252,7 +250,7 @@ export default class DispatchMap extends React.Component {
 		this.updateMap();
 	}
 
-	/**
+	/** 
 	 * Clears callout if exists
 	 */
 	clearCallout() {
@@ -318,7 +316,7 @@ export default class DispatchMap extends React.Component {
 				this.updateUserMarkers(formattedUsers, (markers) => {
 					changedState.userMarkers = markers;
 					changedState.users = formattedUsers;
-
+					
 					this.setState(changedState);
 				});
 			});
@@ -355,7 +353,7 @@ export default class DispatchMap extends React.Component {
 		var keys = Object.keys(newUsers),
 			markers = _.clone(this.state.userMarkers);
 
-		//Make keys after the loop, because keys may have been deleted
+		//Make keys after the loop, because keys may have been deleted 
 		var currentUsers = _.clone(this.state.users),
 			currentUserKeys = Object.keys(currentUsers);
 
@@ -375,7 +373,7 @@ export default class DispatchMap extends React.Component {
 				}
 			}
 			//If the user doesn't exist in the new data set
-			else {
+			else { 
 				//If the marker exists, but the user doesn't, remove the marker and delete from current set
 				if(markers[key]){
 					markers[key].setMap(null);
@@ -388,7 +386,7 @@ export default class DispatchMap extends React.Component {
 			}
 		}
 
-		//If the length changed, reset the keys to avoid
+		//If the length changed, reset the keys to avoid 
 		//iterating over keys that do no exist any more
 		if(currentUsers.length !== this.state.users.length)
 			currentUserKeys = Object.keys(currentUsers);
@@ -408,7 +406,7 @@ export default class DispatchMap extends React.Component {
 	}
 
 	/**
-	 * Adds passed array of assignments to the map,
+	 * Adds passed array of assignments to the map, 
 	 * then sets state from concatted response from `addAssignmentToMap` on each assignment
 	 */
 	addAssignmentsToMap(assignments){
@@ -416,11 +414,11 @@ export default class DispatchMap extends React.Component {
 			circles = [];
 
 		for (var i = 0; i < assignments.length; i++) {
-
+			
 			var mapData = this.addAssignmentToMap(assignments[i], false);
-
+			
 			if(typeof(mapData) === 'undefined') continue;
-
+			
 			//Push into local marker and circles
 			markers.push(mapData.marker);
 			circles.push(mapData.circle);
@@ -442,17 +440,17 @@ export default class DispatchMap extends React.Component {
 		//Lat/Lng will default to center if for a created assignment
 		var map = this.state.map,
 			title = assignment.title || 'No title',
-			zIndex,
+			zIndex, 
 			status,
 			position = new google.maps.LatLng(
-						assignment.location.geo.coordinates[1],
+						assignment.location.geo.coordinates[1], 
 						assignment.location.geo.coordinates[0]
 					  ),
 			radius = assignment.location.radius;
 
 		//Check if the assignment is expired
 		if (assignment.expiration_time && assignment.expiration_time < Date.now()) {
-
+			
 			status = 'expired';
 			zIndex = 100;
 
@@ -474,31 +472,31 @@ export default class DispatchMap extends React.Component {
 		}
 
 		//Check if the status matches the view mode
-		if(status != this.props.viewMode)
+		if(status != this.props.viewMode) 
 			return;
 
 		//Create the rendered circle
 		var circle = this.addCircle(
-			position,
-			global.milesToMeters(radius),
+			position, 
+			global.milesToMeters(radius), 
 			status,
 			assignment._id
 		);
-
+		
 		//Create the marker
 		var marker = this.addAssignmentMarker(
-			position,
+			position, 
 			title,
 			status,
-			zIndex,
+			zIndex, 
 			draggable,
 			assignment._id
 		);
 
 		//Add event handler to display callout when clicekd
 		google.maps.event.addListener(
-			marker,
-			'click',
+			marker, 
+			'click',  
 			this.focusOnAssignment.bind(null, assignment)
 		);
 
@@ -519,9 +517,9 @@ export default class DispatchMap extends React.Component {
 				origin: new google.maps.Point(0, 0),
 				anchor: new google.maps.Point(18, 19)
 			},
-
+			
 			position = position || {lat: this.state.map.getCenter().lat(), lng: this.state.map.getCenter().lng()},
-
+			
 			marker = new google.maps.Marker({
 				position: position,
 				map: this.state.map,
@@ -543,7 +541,7 @@ export default class DispatchMap extends React.Component {
 	 * @param {Assignment status} status active/pending/expired
 	 */
 	addCircle(center, radius, status, assignmentId) {
-
+		
 		var fillColor = global.assignmentColor[status],
 			circle =  new google.maps.Circle({
 				map: this.state.map,
@@ -598,24 +596,10 @@ export default class DispatchMap extends React.Component {
 		if(this.state.activeCallout)
 			this.clearCallout();
 
-		var center = {
+		map.panTo({
 			lat: lat,
 			lng: lng
-		}
-
-		var zoom = this.getAssignmentZoom(assignment);
-
-		// Zoom out happens before pan, which happens before zoom in
-		if (map.getZoom() < zoom) {
-			this.smoothPan(center).then(() => {
-				this.smoothZoom(zoom);
-			});
-		}
-		else {
-			this.smoothZoom(zoom).then(() => {
-				this.smoothPan(center);
-			});
-		}
+		});
 
 		var calloutContent = ReactDOM.renderToString(
 			<DispatchMapCallout assignment={assignment} onClick />
@@ -636,7 +620,7 @@ export default class DispatchMap extends React.Component {
 		});
 
 		callout.open(map);
-
+		
 		this.setState({
 			activeCallout: callout
 		});
@@ -653,143 +637,5 @@ export default class DispatchMap extends React.Component {
 				</div>
 			</div>
 		);
-	}
-
-	/**
-	 * "Smoothly" pans to a new location.
-	 * @param  {LatLng} to Where to pan to
-	 * @return {Promise}   A promise that resolves when the panning is done
-	 */
-	smoothPan(to) {
-		var map = this.state.map;
-
-		var curLat = map.getCenter().lat();
-		var curLng = map.getCenter().lng();
-
-		var panPath = [];
-
-		// Subdivide the length of the pan by the screen size, so each pan
-		// should (theoretically) pan smoothly
-		var span = map.getBounds().toSpan();
-		var latSteps = Math.ceil(Math.abs(curLat - to.lat) / span.lat()) + 1;
-		var lngSteps = Math.ceil(Math.abs(curLng - to.lng) / span.lng()) + 1;
-		var STEPS = Math.max(latSteps, lngSteps);
-
-		var dLat = (to.lat - curLat)/STEPS;
-		var dLng = (to.lng - curLng)/STEPS;
-
-		for (var i = 1; i < STEPS; i++) {
-			panPath.push({
-				lat: curLat + (dLat * i),
-				lng: curLng + (dLng * i)
-			});
-		}
-		panPath.push(to);
-
-		return new Promise((resolve, reject) => {
-			doPan();
-
-			function doPan() {
-				var next = panPath.shift();
-				if (next) {
-					// I feel like there is a way to do this without removing
-					// the listener each time, but I couldn't get it to work.
-					var z = google.maps.event.addListener(map, 'center_changed', function(event){
-						google.maps.event.removeListener(z);
-						setTimeout(doPan, 80);
-					});
-
-					map.panTo(next);
-					// setTimeout(doPan, 80);
-				}
-				else {
-					resolve();
-				}
-			}
-		});
-	}
-
-	/**
-	 * "Smoothly" zooms to a new zoom level
-	 * @param  {int}    target The level to zoom to
-	 * @return {Promise}       A promise that resolves when the zoom is finished
-	 */
-	smoothZoom (target) {
-		var map = this.state.map;
-		var current = map.getZoom();
-
-		return new Promise((resolve, reject) => {
-			doZoom(target, current);
-
-			function doZoom(target, current) {
-				if (current == target) {
-					setTimeout(() => {
-						map.setZoom(current);
-						resolve();
-					}, 80);
-				}
-				else {
-					// I feel like there is a way to do this without removing
-					// the listener each time, but I couldn't get it to work.
-					var z = google.maps.event.addListener(map, 'zoom_changed', function(event){
-
-						var change = target > current ? 1 : -1
-						google.maps.event.removeListener(z);
-						doZoom(target, current + change);
-					});
-					setTimeout(() => {map.setZoom(current)}, 80);
-				}
-			}
-		})
-	}
-
-	getAssignmentZoom(assignment) {
-		var lat = assignment.location.geo.coordinates[1];
-		var lng = assignment.location.geo.coordinates[0];
-		var center = {
-			lat: lat,
-			lng: lng
-		}
-		var radius = global.milesToMeters(assignment.location.radius);
-
-		var circle = new google.maps.Circle({
-			map: null,
-			center: center,
-			radius: radius
-		});
-		var bounds = circle.getBounds();
-
-		var canvas = document.getElementById('map-canvas');
-		var mapDim = {
-			height: canvas.clientHeight,
-			width: canvas.clientWidth
-		}
-
-
-		var WORLD_DIM = { height: 256, width: 256 };
-		var ZOOM_MAX = 21;
-
-		function latRad(lat) {
-			var sin = Math.sin(lat * Math.PI / 180);
-			var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
-			return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
-		}
-
-		function zoom(mapPx, worldPx, fraction) {
-			return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
-		}
-
-		var ne = bounds.getNorthEast();
-		var sw = bounds.getSouthWest();
-
-		var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
-
-		var lngDiff = ne.lng() - sw.lng();
-		var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
-
-		var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
-		var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
-
-		return Math.min(latZoom, lngZoom, ZOOM_MAX);
 	}
 }
