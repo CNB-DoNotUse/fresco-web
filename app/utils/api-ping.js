b40ffcb6e5e57b52ad986c2fe31fb2dca22c7efd
Nolan Leung <nolan@fresconews.com>
2016-03-28 14:26:51 -0400

    Place ping in app.ejs

--- app/utils/api-ping.js
+++
@@ -1,9 +0,0 @@
-(function(){
-	window.setInterval(function() {
-		$.get('/api/ping', function(data) {
-			if(!data.pong) {
-				window.location = '/account?next=' + window.location.pathname;
-			}
-		});
-	}, 15000);
-})();
