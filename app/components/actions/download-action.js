5dbab0c9d5cbd846de2482299f4c7a50975bb82b
Elmir Kouliev <elmir@kouliev.com>
2016-03-30 16:06:18 -0400

    click event override for non-supported browsers

--- app/components/actions/download-action.js
+++ app/components/actions/download-action.js
@@ -8,21 +8,27 @@ export default class DownloadAction extends React.Component {
 
 	constructor(props) {
 		super(props);
+
 		this.download = this.download.bind(this);
 	}
 
 	render() {
 
 		return (
-
 			<span className="mdi mdi-download icon pull-right" onClick={this.download}></span>
-
 		);
 		
 	} 
 
 	//Called whenever the purhcase icon is selected
 	download(event) {
+
+		//Override click event for browsers that do not support it
+		HTMLElement.prototype.click = function() {
+		    var evt = this.ownerDocument.createEvent('MouseEvents');
+		    evt.initMouseEvent('click', true, true, this.ownerDocument.defaultView, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
+		    this.dispatchEvent(evt);
+		} 
 
 		if(!this.props.post){
 			
