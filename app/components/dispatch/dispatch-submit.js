a72309c10586b2c9c9c1b0aa89a203b53d92a12a
Elmir Kouliev <elmir@kouliev.com>
2016-04-16 17:04:36 -0400

    Changed timeout

--- app/components/dispatch/dispatch-submit.js
+++ app/components/dispatch/dispatch-submit.js
@@ -64,7 +64,7 @@ export default class DispatchSubmit extends React.Component {
 					setTimeout(() => {					
 						//recurse in case of a rate limit
 						geocode(geo);
-					}, 1000);
+					}, 300);
 				}
 			});
 		}
