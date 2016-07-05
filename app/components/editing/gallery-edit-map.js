f25c418b06aff7a3bbfebed81c482a848643e3d8
Evan Rubinton <evanrubinton@gmail.com>
2016-07-05 14:29:41 -0400

    update galleryEditMap syntax

--- app/components/editing/gallery-edit-map.js
+++ app/components/editing/gallery-edit-map.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import AutocompleteMap from '../global/autocomplete-map'
+import React from 'react';
+import AutocompleteMap from '../global/autocomplete-map';
 
 /**
  * Component for managing gallery map representation
@@ -7,48 +7,45 @@ import AutocompleteMap from '../global/autocomplete-map'
 
 export default class GalleryEditMap extends React.Component {
 
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-        let location = this.props.gallery.location
-        ? this.props.gallery.location.coordinates
-        ? this.props.gallery.location.coordinates[0]
-        : this.props.gallery.location : null;
-
+    render() {
+        const { gallery, onPlaceChange } = this.props;
+        let location = null;
         let defaultLocation = '';
 
-		if(this.props.gallery.address) {
-			defaultLocation = this.props.gallery.address;
-		} else if(this.props.gallery.posts[0].location) {
-			defaultLocation = this.props.gallery.posts[0].location.address
-		} else {
-			defaultLocation = null;
-		}
+        if (gallery.location) {
+            location = gallery.location.coordinates
+            ? gallery.location.coordinates[0]
+            : gallery.location;
+        }
 
-		if(Array.isArray(location)) {
-			if(!location[0].lat) {
-				location = location.map((loc) => {
-					return {
-						lat: loc[1],
-						lng: loc[0]
-					}
-				});
-			}
-		}
+        if (gallery.address) {
+            defaultLocation = gallery.address;
+        } else if (gallery.posts[0].location) {
+            defaultLocation = gallery.posts[0].location.address;
+        } else {
+            defaultLocation = null;
+        }
 
-		return (
-			<div className="dialog-col col-xs-12 col-md-5 pull-right">
+        if (Array.isArray(location)) {
+            if (!location[0].lat) {
+                location = location.map((loc) => (
+                    { lat: loc[1], lng: loc[0] }
+                ));
+            }
+        }
+
+        return (
+            <div className="dialog-col col-xs-12 col-md-5 pull-right">
                 <AutocompleteMap
                     defaultLocation={defaultLocation}
                     location={location}
                     hasRadius={false}
-                    onPlaceChange={this.props.onPlaceChange}
-                    disabled={this.props.gallery.owner}
-                    rerender={true}
+                    onPlaceChange={onPlaceChange}
+                    disabled={gallery.owner}
+                    rerender
                 />
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 }
+
