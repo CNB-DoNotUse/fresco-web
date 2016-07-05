a7668f14fa06d3c4e9966576f079de4e93f88317
Evan Rubinton <evanrubinton@gmail.com>
2016-07-05 16:38:17 -0400

    update outletSettings cmp, temp remove outlet-members cmp rendering(until api supports)

--- app/views/outletSettings.js
+++ app/views/outletSettings.js
@@ -1,98 +1,97 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
-import TopBar from '../components/topbar'
-import OutletBody from '../components/outlet/outlet-body'
-import OutletInfo from '../components/outlet/outlet-info'
-import OutletPaymentInfo from '../components/outlet/outlet-payment-info'
-import QuickSupport from '../components/global/quick-support'
-import OutletMembers from '../components/outlet/outlet-members'
-import OutletLocations from '../components/outlet/outlet-locations'
-import OutletNotifications from '../components/outlet/outlet-notifications'
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import clone from 'lodash/clone';
+import App from './app';
+import TopBar from '../components/topbar';
+import OutletInfo from '../components/outlet/outlet-info';
+import OutletPaymentInfo from '../components/outlet/outlet-payment-info';
+import QuickSupport from '../components/global/quick-support';
+import OutletMembers from '../components/outlet/outlet-members';
+import OutletLocations from '../components/outlet/outlet-locations';
+import OutletNotifications from '../components/outlet/outlet-notifications';
 
 /**
  * Outlet Settings page
  */
 
 class OutletSettings extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            outlet: this.props.outlet,
+        };
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			outlet: this.props.outlet,
-			user: this.props.user
-		}
+        this.updateMembers = this.updateMembers.bind(this);
+        this.updateOutlet = this.updateOutlet.bind(this);
+    }
 
-		this.updateMembers = this.updateMembers.bind(this);
-		this.updateOutlet = this.updateOutlet.bind(this);
-	}
+    updateMembers(users) {
+        const outlet = clone(this.state.outlet);
+        outlet.users = users;
 
-	updateMembers(users) {
-		var outlet = _.clone(this.state.outlet);
-		outlet.users = users;
-		
-		this.setState({
-			outlet: outlet,
-		});
-	}
+        this.setState({ outlet });
+    }
 
-	updateOutlet(outlet) {
-		var user = _.clone(this.state.user);
-		user.outlet = outlet;
-		
-		this.setState({
-			outlet: outlet,
-			user: user
-		});
-	}
+    updateOutlet(outlet) {
+        this.setState({ outlet });
+    }
 
-	render() {
+    render() {
+        const { user } = this.props;
+        const isOwner = user.permissions.includes('update-outlet');
+        const className = `outlet-settings ${!isOwner ? 'centered' : ''}`;
+        let members = '';
+        let left = '';
 
-		var isOwner= this.state.user.outlet.owner == this.state.user.id,
-			className = 'outlet-settings' + (!isOwner ? ' centered' : ''),
-			members = '',
-			left = '';
+        if (isOwner) {
+            left = (
+                <div className="left">
+                    <OutletInfo
+                        updateOutlet={this.updateOutlet}
+                        outlet={this.state.outlet}
+                    />
+                    <OutletPaymentInfo outlet={this.state.outlet} />
+                </div>
+            );
+            // members = (
+            //     <OutletMembers
+            //         outlet={this.state.outlet}
+            //         updateMembers={this.updateMembers}
+            //         members={this.state.outlet.users}
+            //     />
+            // );
+        }
 
-		if(isOwner){
-			left = <div className="left">
-						<OutletInfo 
-							updateOutlet={this.updateOutlet}
-							outlet={this.state.outlet} />
+        return (
+            <App user={user}>
+                <TopBar
+                    title={this.state.outlet.title}
+                />
 
-						<OutletPaymentInfo outlet={this.state.outlet} />
-					</div>
+                <div className={className}>
+                    {left}
+                    <div className="right">
+                        <OutletNotifications outlet={this.state.outlet} />
+                        <OutletLocations outlet={this.state.outlet} />
+                        {members ? members : ''}
+                    </div>
 
-			members = <OutletMembers
-							outlet={this.state.outlet}
-							updateMembers={this.updateMembers}
-							members={this.state.outlet.users} />
-		}
-
-		return (
-			<App user={this.state.user}>
-				<TopBar
-					title={this.state.outlet.title} />
-				
-				<div className={className}>
-					{left}
-					<div className="right">
-						<OutletNotifications outlet={this.state.outlet} />	
-
-						<OutletLocations outlet={this.state.outlet} />	
-						
-						{members}
-					</div>
-
-					<QuickSupport />
-				</div>
-			</App>
-		)
-	}
+                    <QuickSupport />
+                </div>
+            </App>
+        );
+    }
 }
 
+OutletSettings.propTypes = {
+    user: PropTypes.object,
+    outlet: PropTypes.object,
+};
+
 ReactDOM.render(
-  <OutletSettings
-  	user={window.__initialProps__.user}
-  	outlet={window.__initialProps__.outlet} />,
+    <OutletSettings
+        user={window.__initialProps__.user}
+        outlet={window.__initialProps__.outlet}
+    />,
 	document.getElementById('app')
 );
