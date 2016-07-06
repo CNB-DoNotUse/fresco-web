ab7c454ccc59e70487374da87b172dfe328dab2f
Evan Rubinton <evanrubinton@gmail.com>
2016-07-06 15:48:13 -0400

    update post-detail-image for new purchased bool handling

--- app/components/postDetail/post-detail-image.js
+++ app/components/postDetail/post-detail-image.js
@@ -1,7 +1,7 @@
-import React from 'react'
-import PurchaseAction from '../actions/purchase-action.js'
-import DownloadAction from '../actions/download-action.js'
-import global from '../../../lib/global'
+import React, { PropTypes } from 'react';
+import PurchaseAction from '../actions/purchase-action.js';
+import DownloadAction from '../actions/download-action.js';
+import global from '../../../lib/global';
 
 /** //
 
@@ -12,94 +12,100 @@ Description : Image of the PostDetail page, contains image/video, byline and act
 /**
  * Post Detail Image parent object
  */
+class PostDetailImage extends React.Component {
+    constructor(props) {
+        super(props);
 
-export default class PostDetailImage extends React.Component {
+        const purchased = props.post.purchased
+            ? props.post.purchased !== 0
+            : false;
+        this.state = { purchased };
+        this.contextMenu = this.contextMenu.bind(this);
+    }
 
-	constructor(props) {
-		super(props);
+    onPurchase() {
+        this.setState({ purchased: true });
+    }
 
-		this.state = {
-			purchased: this.props.purchases && this.props.purchases.indexOf(this.props.post.id) >= 0
-		}
-
-		this.contextMenu = this.contextMenu.bind(this);
-		this.didPurchase = this.didPurchase.bind(this);
-	}
-
-	didPurchase() {
-		this.setState({
-			purchased: true
-		});
-	}
-
-	/**
+    /**
 	 * Click event for either the image tag or the video tag
 	 */
-	contextMenu(e) {
-		e.preventDefault();
-	}
+    contextMenu(e) {
+        e.preventDefault();
+    }
 
     render() {
-        const { user, post, didPurchase } = this.props;
-		let actions = [];
-		let postMedia = '';
-		let i = 0;
+        const { user, post } = this.props;
+        let actions = [];
+        let postMedia = '';
+        let i = 0;
 
-		var assignment = window.location.search.split('assignment=')[1];
+        const assignment = window.location.search.split('assignment=')[1];
+        const downloadAction = <DownloadAction post={post} key={i++} />;
+        const purchaseAction = (
+            <PurchaseAction
+                post={post}
+                assignment={assignment}
+                onPurchase={(bool) => this.onPurchase(bool)}
+                key={i++}
+            />
+        );
 
-		var downloadAction = <DownloadAction post={post} key={i++} />
-
-		var purchaseAction = <PurchaseAction
-								post={post}
-								assignment={assignment}
-								didPurchase={didPurchase}
-								key={i++} />
-
-        if(this.props.user.rank < global.RANKS.CONTENT_MANAGER){
-
-			if(user.outlet && this.state.purchased){
+        if (user.rank < global.RANKS.CONTENT_MANAGER) {
+            if (user.outlet && this.state.purchased) {
                 actions.push(downloadAction);
-				//Check if the post is licensed
-            } else if (user.outlet && post.license == 1){
+            } else if (user.outlet && post.license === 1) {
+                // Check if the post is licensed
                 actions.push(purchaseAction);
-			}
+            }
         } else {
             actions.push(downloadAction);
 
-            if (!this.state.purchased){
+            if (!this.state.purchased) {
                 actions.push(purchaseAction);
             }
         }
 
-		if (post.video){
-			postMedia = <video width="100%" height="100%" controls onContextMenu={this.contextMenu}>
-							<source src={global.formatVideo(post.video)} type="video/mp4" />
-							Your browser does not support the video tag.
-						</video>
-		}
-		else{
-            postMedia = <img
-                            className="img-responsive"
-                            onContextMenu={this.contextMenu}
-                            src={global.formatImg(post.image, 'large')}
-                        />
-		}
+        if (post.video) {
+            postMedia = (
+                <video width="100%" height="100%" controls onContextMenu={this.contextMenu}>
+                    <source src={global.formatVideo(post.video)} type="video/mp4" />
+                    Your browser does not support the video tag.
+                </video>
+            );
+        } else {
+            postMedia = (
+                <img
+                    role="presentation"
+                    className="img-responsive"
+                    onContextMenu={this.contextMenu}
+                    src={global.formatImg(post.image, 'large')}
+                />
+            );
+        }
 
 
-		return (
+        return (
+            <div className="col-xs-12 col-md-8">
+                <div className="card panel">
+                    <div className="card-foot small">
+                        {actions}
 
-			<div className="col-xs-12 col-md-8">
-				<div className="card panel">
-					<div className="card-foot small">
-						{actions}
+                        <span className="md-type-body1">{post.byline}</span>
+                    </div>
 
-						<span className="md-type-body1">{post.byline}</span>
-					</div>
+                    <div className="card-body">{postMedia}</div>
+                </div>
+            </div>
 
-					<div className="card-body">{postMedia}</div>
-				</div>
-			</div>
-
-		)
-	}
+        );
+    }
 }
+
+PostDetailImage.propTypes = {
+    post: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+};
+
+export default PostDetailImage;
+
