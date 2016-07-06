2796ff9a300c79fdd48f984bae7fbc39215b622d
Evan Rubinton <evanrubinton@gmail.com>
2016-07-06 14:28:59 -0400

    remove use of purchases array from routes/archive

--- routes/archive.js
+++ routes/archive.js
@@ -1,10 +1,6 @@
-var express     = require('express'),
-    config      = require('../lib/config'),
-    Purchases   = require('../lib/purchases'),
-    global      = require('../lib/global'),
-    request     = require('request-json'),
-    router      = express.Router(),
-    api         = request.createClient(config.API_URL)
+const express = require('express');
+const config = require('../lib/config');
+const router = express.Router();
 
 /** //
 
@@ -16,64 +12,51 @@ var express     = require('express'),
  * Index Content Page
  */
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
+    const title = 'Archive';
+    const props = { user: req.session.user, title };
 
-  var title = 'Archive',
-      props = {
-        user : req.session.user,
-        purchases : Purchases.mapPurchases(req.session),
-        title: title
-      };
-
-  res.render('app', {
-    title: title,
-    page : 'archive',
-    alerts: req.alerts,
-    props : JSON.stringify(props)
-  });
-
+    res.render('app', {
+        title,
+        page: 'archive',
+        alerts: req.alerts,
+        props: JSON.stringify(props),
+    });
 });
 
 /**
  * Page for all galleries
  */
 
-router.get('/galleries', (req, res, next) => {
+router.get('/galleries', (req, res) => {
     const title = 'Galleries';
-    const props = {
-        user: req.session.user,
-        title,
-    };
+    const props = { user: req.session.user, title };
 
     res.render('app', {
         user: req.session.user,
-        title: title,
-        config: config,
+        title,
+        config,
         alerts: req.alerts,
         page: 'galleries',
-        props : JSON.stringify(props)
+        props: JSON.stringify(props),
     });
-
 });
 
 /**
  * Page for all stories
  */
 
-router.get('/stories', (req, res, next) => {
-  var title = 'Stories',
-      props = {
-        user : req.session.user
-      };
+router.get('/stories', (req, res) => {
+    const title = 'Stories';
+    const props = { user: req.session.user };
 
-  res.render('app', {
-    title: title,
-    config: config,
-    alerts: req.alerts,
-    page: 'stories',
-    props : JSON.stringify(props)
-  });
-
+    res.render('app', {
+        title,
+        config,
+        alerts: req.alerts,
+        page: 'stories',
+        props: JSON.stringify(props),
+    });
 });
 
 /**
@@ -81,28 +64,24 @@ router.get('/stories', (req, res, next) => {
  * @param {string} filter Filter of content type i.e. videos/photos
  */
 
-router.get('/:filter', (req, res, next) => {
-  var filters = ['photos', 'videos'];
+router.get('/:filter', (req, res) => {
+    const filters = ['photos', 'videos'];
 
-  // Check if filter is valid
-  if(filters.indexOf(req.params.filter.toLowerCase()) == -1) {
-    return res.redirect('/');
-  }
+    // Check if filter is valid
+    if (filters.indexOf(req.params.filter.toLowerCase()) === -1) {
+        return res.redirect('/');
+    }
 
-  var props = {
-    user : req.session.user,
-    purchases : Purchases.mapPurchases(req.session)
-  },
-  title = req.params.filter[0].toUpperCase() + req.params.filter.slice(1);
+    const props = { user: req.session.user };
+    const title = req.params.filter[0].toUpperCase() + req.params.filter.slice(1);
 
-  res.render('app', {
-    title: title,
-    page : req.params.filter,
-    alerts: req.alerts,
-    props : JSON.stringify(props)
-  });
-
+    return res.render('app', {
+        title,
+        page: req.params.filter,
+        alerts: req.alerts,
+        props: JSON.stringify(props),
+    });
 });
 
-
 module.exports = router;
+
