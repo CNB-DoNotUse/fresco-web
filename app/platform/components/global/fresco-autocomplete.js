c6c81aadc46b7ebbf5d30057c3a20fb694ce9af6
Evan Rubinton <evanrubinton@gmail.com>
2016-07-15 13:32:39 -0400

    bug fix/clean up fresco autocomplete cmp

--- app/platform/components/global/fresco-autocomplete.js
+++ app/platform/components/global/fresco-autocomplete.js
@@ -1,56 +1,41 @@
-import React from 'react';
-import FrescoImage from './fresco-image';
-import utils from 'utils'
+import React, { PropTypes } from 'react';
 
 /**
  * Autocomplete component
  */
 
-export default class FrescoAutocomplete extends React.Component {
-
+class FrescoAutocomplete extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            inputText: this.props.inputText,
-            predictions: []
-        }
+        this.state = { inputText: this.props.inputText, predictions: [] };
 
-        //Instantiate Google Maps services
+        // Instantiate Google Maps services
         this.geocoder = new google.maps.Geocoder();
         this.autoCompleteService = new google.maps.places.AutocompleteService();
         this.placesService = new google.maps.places.PlacesService(document.createElement('span'));
 
-        this.predictionSelected = this.predictionSelected.bind(this);
         this.calculateCrossStreets = this.calculateCrossStreets.bind(this);
     }
 
     componentDidMount() {
-        if(this.props.inputText) {
+        if (this.props.inputText) {
             this.refs.inputField.value = this.props.inputText;
         }
 
-        //Click event for outside clicking
+        // Click event for outside clicking
         $(document).click((e) => {
-            if ($(e.target).parents('.autocomplete').size() == 0 && e.target !== this.refs.autocompleteWrap) {
-                //Reset predictions for cleanup
-                this.setState({
-                    predictions: [],
-                    active: false
-                });
+            if ($(e.target).parents('.autocomplete').size() === 0 && e.target !== this.refs.autocompleteWrap) {
+                // Reset predictions for cleanup
+                this.setState({ predictions: [], active: false });
             }
         });
     }
 
-    componentWillUnmount() {
-        //Clean up click event on unmount
-        $(document).unbind('click');
-    }
-
     componentWillReceiveProps(nextProps) {
-        //Check if the passed input text has changed
-        if(nextProps.inputText !== null){
-            if(nextProps.inputText !== this.state.inputText){
+        // Check if the passed input text has changed
+        if (nextProps.inputText !== null) {
+            if (nextProps.inputText !== this.state.inputText) {
                 this.refs.inputField.value = nextProps.inputText;
                 this.refs.inputField.focus();
 
@@ -60,32 +45,32 @@ export default class FrescoAutocomplete extends React.Component {
         }
     }
 
+    componentWillUnmount() {
+        // Clean up click event on unmount
+        $(document).unbind('click');
+    }
+
     /**
      * Input event handler
      */
-    inputChanged(e) {
+    inputChanged() {
         const field = this.refs.inputField;
         const query = field.value;
 
         if (query === '' || query == null) {
-            return this.setState({
-                predictions: [],
-                active: false
-            });
+            this.setState({ predictions: [], active: false });
+            return;
         }
 
         this.setState({ active: true });
-
-        var params = { input: query }
+        const params = { input: query };
 
         this.autoCompleteService.getPlacePredictions(params, (predictions, status) => {
             if (status === google.maps.GeocoderStatus.OK && typeof(predictions) !== 'undefined') {
-                this.setState({
-                    predictions: predictions
-                });
+                this.setState({ predictions });
             } else {
-                //Calculate cross streets if no suggestions from google maps
-                return this.calculateCrossStreets(query);
+                // Calculate cross streets if no suggestions from google maps
+                this.calculateCrossStreets(query);
             }
         });
     }
@@ -94,21 +79,17 @@ export default class FrescoAutocomplete extends React.Component {
      * Calculates cross street in area using geocoder, called if no results from place suggestions
      */
     calculateCrossStreets(query) {
-        var params = { address: query }
+        const params = { address: query };
 
-        if(this.props.bounds) {
+        if (this.props.bounds) {
             params.bounds = this.props.bounds;
         }
 
         this.geocoder.geocode(params, (predictions, status) => {
-            if(status === google.maps.GeocoderStatus.OK && predictions[0]){
-                this.setState({
-                    predictions: predictions
-                });
+            if (status === google.maps.GeocoderStatus.OK && predictions[0]) {
+                this.setState({ predictions });
             } else {
-                this.setState({
-                    predictions: []
-                })
+                this.setState({ predictions: [] });
             }
         });
     }
@@ -117,69 +98,58 @@ export default class FrescoAutocomplete extends React.Component {
      * OnClick for selecting a prediction from the list
      */
     predictionSelected(prediction) {
-        var self = this;
+        const updateAssignment = () => {
+            const location = {
+                lat: prediction.geometry.location.lat(),
+                lng: prediction.geometry.location.lng(),
+            };
 
-        if(typeof(prediction.geometry) === 'undefined') {
-            this.placesService.getDetails({
-                reference: prediction.reference
-            }, (details, status) => {
-                //Inherit description from initial object
-                details.description = prediction.description;
+            this.setState({ predictions: [], active: false });
 
-                //Set prediciton to details result
-                prediction = details;
-
-                updateAssignment();
+            this.props.updateAutocompleteData({
+                prediction,
+                location,
+                address: prediction.description || prediction.formatted_address,
             });
+        };
+
+        if (typeof(prediction.geometry) === 'undefined') {
+            this.placesService.getDetails({ reference: prediction.reference },
+                (details) => {
+                    // Inherit description from initial object
+                    details.description = prediction.description;
+
+                    // Set prediciton to details result
+                    prediction = details;
+
+                    updateAssignment();
+                });
         } else {
             updateAssignment();
         }
+    }
 
-        function updateAssignment() {
-            var location = {
-                lat: prediction.geometry.location.lat(),
-                lng: prediction.geometry.location.lng()
-            };
+    renderPredictions() {
+        const { predictions } = this.state;
+        if (!predictions.length) return '';
 
-            self.setState({
-                predictions: [],
-                active: false
-            });
-
-            self.props.updateAutocompleteData({
-                prediction: prediction,
-                location: location,
-                address: prediction.description || prediction.formatted_address
-            });
-        }
+        return (
+            <ul className="predictions">
+                {
+                    predictions.map((prediction, i) => (
+                        <li onClick={() => this.predictionSelected(prediction)} key={i}>
+                            <p>{prediction.description}</p>
+                        </li>
+                    ))
+                }
+            </ul>
+        );
     }
 
     render() {
-        let predictionsDropdown = '',
-            autocompleteClass = '';
+        let autocompleteClass = `autocomplete ${this.props.class}`;
 
-        if(this.state.predictions.length !== 0) {
-            let predictions = this.state.predictions.map((prediction, i) => {
-                let text = '';
-
-                if(typeof(prediction.description) !== 'undefined') {
-                    text = prediction.description;
-                } else if(typeof(prediction.formatted_address) !== 'undefined') {
-                    text = prediction.formatted_address;
-                }
-
-                return <li onClick={this.predictionSelected.bind(this, prediction)}
-                            key={i} >
-                            <p>{text}</p>
-                        </li>
-            });
-
-            predictionsDropdown = <ul className="predictions">{predictions}</ul>
-        }
-
-        autocompleteClass = 'autocomplete ' +  this.props.class
-
-        if(this.state.active && this.props.transition) {
+        if (this.state.active && this.props.transition) {
             autocompleteClass += ' active';
         }
 
@@ -197,15 +167,27 @@ export default class FrescoAutocomplete extends React.Component {
                     />
                 </div>
 
-                {predictionsDropdown}
+                {this.renderPredictions()}
             </div>
-        )
+        );
     }
 }
 
 FrescoAutocomplete.defaultProps = {
-    updateAutocompleteData: function () {},
+    updateAutocompleteData() {},
     transition: true,
     class: '',
-    inputClass: ''
-}
+    inputClass: '',
+};
+
+FrescoAutocomplete.propTypes = {
+    class: PropTypes.string,
+    inputText: PropTypes.string,
+    inputClass: PropTypes.string,
+    disabled: PropTypes.bool,
+    transition: PropTypes.bool,
+    updateAutocompleteData: PropTypes.func,
+};
+
+export default FrescoAutocomplete;
+
