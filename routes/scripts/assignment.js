16f46901718e29b7801ded7a059c4522f6d86ccf
Elmir Kouliev <elmir@kouliev.com>
2016-07-21 11:44:20 -0400

    Stats endpoint cleanup

--- routes/scripts/assignment.js
+++ routes/scripts/assignment.js
@@ -2,84 +2,32 @@ var express = require('express'),
     config = require('../../lib/config'),
     global = require('../../lib/global'),
     API = require('../../lib/api'),
-    xlsx = require('node-xlsx'),
+    csv = require('../../lib/csv'),
     router = express.Router();
 	
-//---------------------------vvv-ASSIGNMENT-ENDPOINTS-vvv---------------------------//
-router.get('/assignment/stats', (req, res) => {
-  // if(!req.session.user || req.session.user.rank < 1) return res.status(403).send({ err: 'Unauthorized' });
+router.get('/assignment/stats', (req, res, next) => {
+    API.request({
+        method: 'GET',
+        url: '/assignment/stats',
+        token: 'c328b663-42f9-48e3-bff7-195d800329d5'
+    }, (err, response) => {
+        if(err) {
+            return next({ 
+                message: 'Error downloading stats!', 
+                status: 500 
+            });        
+        }
 
-  API.request({
-    method: 'GET',
-    url: '/assignment/stats',
-    token: req.session.token
-  }, (err, resp) => {
-    if(err || resp.status != 200) return res.send({err: 'API Error'});
+        csv(response.body, (err, csv) => {
+            if(err) {
+                return next({ message: err, status: 500 });
+            }
 
-    let assignments = [],
-        xlsxData = [[
-          'Assignment ID',
-          'Outlet ID',
-          'Outlet',
-          'Creator',
-          'Transferred',
-          'Title',
-          'Address',
-          'Zip Code',
-          'Radius',
-          'Day',
-          'Date Created',
-          'Time Created',
-          'Users',
-          'Submissions',
-          'Photos',
-          'Photos Purchased',
-          'Videos',
-          'Videos Purchased',
-          'First Submission'
-        ]];
-
-    try {
-
-      assignments = JSON.parse(resp.text);
-
-    } catch (e) {
-      return res.send({ err: 'API Parse Error' });
-    }
-
-    for(let a of assignments) {
-
-      xlsxData.push([
-        a.id,
-        a.outlet_id,
-        a.outlet_name,
-        a.creator,
-        a.transferred,
-        a.title,
-        a.address,
-        a.zip,
-        a.radius,
-        global.getWeekDay(a.day_created),
-        a.date_created,
-        a.time_created,
-        a.users,
-        a.submissions,
-        a.photos,
-        a.photos_purchased,
-        a.videos,
-        a.videos_purchased,
-        a.firstSubmissionTime
-      ]);
-
-    }
-
-    res.set('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
-    res.set('Content-Disposition', 'inline; filename="AssignmentStats.xlsx"')
-    res.send(xlsx.build([{name: 'Assignments', data: xlsxData}]));
-
-  });
-
+            res.set('Content-Type', 'text/csv');
+            res.set('Content-Disposition', 'inline; filename="assignments.csv"');
+            res.send(csv).end();
+        });
+    });
 });
-//---------------------------^^^-ASSIGNMENT-ENDPOINTS-^^^---------------------------//
 
 module.exports = router;
