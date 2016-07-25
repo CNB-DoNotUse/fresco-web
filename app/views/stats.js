0a48642502f3eba6ffa504f1126ce1b619303a85
Elmir Kouliev <elmir@kouliev.com>
2016-07-25 16:29:42 -0400

    Updated stats page to take KML

--- app/views/stats.js
+++ app/views/stats.js
@@ -29,10 +29,12 @@ class Stats extends React.Component {
         this.calculateUsers = this.calculateUsers.bind(this);
         this.polygonChanged = this.polygonChanged.bind(this);
         this.placeChanged = this.placeChanged.bind(this);
+        this.kmlClicked = this.kmlClicked.bind(this);
+        this.kmlFileInputChange = this.kmlFileInputChange.bind(this);
     }
 
     componentDidMount() {
-        var mapOptions = {
+        const mapOptions = {
             center: {lat: 40.731080, lng: -73.999365},
             zoom: 9,
             mapTypeControl: false,
@@ -43,25 +45,25 @@ class Stats extends React.Component {
         };
 
         //Instantiate google maps object
-        var map = new google.maps.Map(
+        const map = new google.maps.Map(
             document.getElementById('stat-map'),
             mapOptions
         );
 
-        var autocomplete = new google.maps.places.Autocomplete(this.refs['stats-autocomplete']);
+        const autocomplete = new google.maps.places.Autocomplete(this.refs['stats-autocomplete']);
 
         // Bind place_changed event to placeChanged
         google.maps.event.addListener( autocomplete, 'place_changed', this.placeChanged);
 
         //Define intitial coords
-        var polyCoords = [
+        const polyCoords = [
             {lat: 40.91525559999999, lng: -73.7002721},
             {lat: 40.4960439 , lng: -73.7002721},
             {lat: 40.496043, lng: -74.2557349}
         ];
 
         //Create polygon
-        var polygon = new google.maps.Polygon({
+        const polygon = new google.maps.Polygon({
             map: map,
             paths: polyCoords,
             strokeColor: '#0047bb',
@@ -76,24 +78,87 @@ class Stats extends React.Component {
 
         google.maps.event.addListener(polygon, 'mouseup', this.polygonChanged);
 
-        this.setState({
-            polygon: polygon,
-            map: map,
-            autocomplete: autocomplete
-        });
+        this.setState({ polygon, map, autocomplete });
     }
 
+    //Downloads submission stats by redirectring to API endpoint
     downloadStats() {
         $.snackbar({content: 'Downloading...'});
 
         window.location.replace("/api/stats/submissions");
     }
 
+    //Click event for button that invokes file picker
+    kmlClicked() {
+        this.refs.kmlFileInput.click();
+    }
+
+    /**
+     * File input listener
+     * @description Takes inputed file and parses as KML for a location polygon
+     */
+    kmlFileInputChange() {
+        const file = this.refs.kmlFileInput.files[0];
+        const reader = new FileReader();
+        const { map, polygon } = this.state;
+        const self = this;
+
+        //Check if a KML file
+        if(file.name.indexOf('kml') == -1) {
+            return $.snackbar({content: 'Please upload a KML file!'});
+        }
+
+        reader.onload = () => {
+            //Parse KML file
+            const kml = new DOMParser().parseFromString(reader.result, "text/xml");
+            let pointArray = '';
+
+            try {
+                pointArray = kml
+                .getElementsByTagName("Polygon")[0]
+                .getElementsByTagName("outerBoundaryIs")[0]
+                .getElementsByTagName("coordinates")[0]
+                .innerHTML.split(" ");
+            } catch(e) {
+                return $.snackbar({content: 'There was an error reading this file!'});
+            }
+
+            //Map into coordinates array by parsing string
+            const coordinates = pointArray.map((str) => {
+                return [
+                    str.substring(0, str.indexOf(',')),
+                    str.substring(str.indexOf(',') + 1, str.length)
+                ]
+            });
+
+            handleCoordinates(coordinates);
+        };
+
+        reader.readAsText(file);
+
+        //Take coordinates and send to rest of component to update
+        function handleCoordinates(coordinates) {
+            let bounds = new google.maps.LatLngBounds();
+            const latLngArray = coordinates.map((latLng) => {
+                latLng = new google.maps.LatLng(latLng[1], latLng[0]);
+                //Extend bounds each time
+                bounds.extend(latLng);  
+                return latLng;
+            });
+
+            self.calculateUsers(coordinates);
+            polygon.setPaths(latLngArray);
+            //Update map to bounds of polygon
+            map.fitBounds(bounds);
+        }
+    }
+
     /**
      * Event handler for autocomplete
      */
     placeChanged(event) {
-        var place = this.state.autocomplete.getPlace();
+        const place = this.state.autocomplete.getPlace();
+        const { map, polygon } = this.state;
 
         //Run checks on place and title
         if (!place || !place.geometry || !place.geometry.viewport){
@@ -102,40 +167,39 @@ class Stats extends React.Component {
             return $.snackbar({content: 'Please enter a valid location title'});
         }
 
-        var bounds = place.geometry.viewport,
-            locationPolygon = global.generatePolygonFromBounds(bounds)[0],
-            latLngArray = [
-                new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getNorthEast().lng()),
-                new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getNorthEast().lng()),
-                new google.maps.LatLng(bounds.getSouthWest().lat(), bounds.getNorthEast().lng()),
-                new google.maps.LatLng(bounds.getSouthWest().lat(), bounds.getSouthWest().lng()),
-                new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getSouthWest().lng()),
-                new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getNorthEast().lng())
-            ];
+        let bounds = place.geometry.viewport;
+        let locationPolygon = global.generatePolygonFromBounds(bounds)[0];
+        let latLngArray = [
+            new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getNorthEast().lng()),
+            new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getNorthEast().lng()),
+            new google.maps.LatLng(bounds.getSouthWest().lat(), bounds.getNorthEast().lng()),
+            new google.maps.LatLng(bounds.getSouthWest().lat(), bounds.getSouthWest().lng()),
+            new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getSouthWest().lng()),
+            new google.maps.LatLng(bounds.getNorthEast().lat(), bounds.getNorthEast().lng())
+        ];
 
         this.calculateUsers(locationPolygon);
-
-        this.state.map.fitBounds(bounds);
-        this.state.polygon.setPaths(latLngArray);
+        map.fitBounds(bounds);
+        polygon.setPaths(latLngArray);
     }
 
     /**
      * Event handler from google maps polygon
      */
     polygonChanged(event) {
-        var vertices = this.state.polygon.getPath(),
-            locations = [];
+        const vertices = this.state.polygon.getPath();
+        let locations = [];
 
          // Iterate over the vertices.
-        for (var i =0; i < vertices.getLength(); i++) {
-            var coord = vertices.getAt(i);
+        for (let i =0; i < vertices.getLength(); i++) {
+            const coord = vertices.getAt(i);
 
             locations.push([coord.lng(), coord.lat()]);
         }
 
         //Close-off end of polygon
-        var coord = vertices.getAt(0);
-        locations.push([coord.lng(), coord.lat()]);
+        const firstCoordinate = vertices.getAt(0);
+        locations.push([firstCoordinate.lng(), firstCoordinate.lat()]);
 
         this.calculateUsers(locations);
     }
@@ -144,28 +208,28 @@ class Stats extends React.Component {
      * Calculates users from passed polygon
      */
     calculateUsers(locations) {
-        var query = '';
+        const self = this;
+        const day = 86400000;
+        const timeKeys = Object.keys(this.state.counts);
+        const times = [
+            Date.now() - day, //day
+            Date.now() - (day * 7), //one week
+            Date.now() - (day * 14), //two weeks
+            Date.now() - (day * 31), //one month
+            Date.now() - (day * 365), //year
+            null
+        ];
 
-        for (var i = 0; i < locations.length; i++) {
+        let query = '';
+        let counts = _.clone(this.state.counts)
+
+        for (let i = 0; i < locations.length; i++) {
             if(i > 0) query += '&';
             query += 'coordinates[]=' + locations[i][0] + ',' + locations[i][1];
         }
 
-        var day = 86400000,
-            times = [
-                Date.now() - day, //day
-                Date.now() - (day * 7), //one week
-                Date.now() - (day * 14), //two weeks
-                Date.now() - (day * 31), //one month
-                Date.now() - (day * 365), //year
-                null
-            ],
-            timeKeys = Object.keys(this.state.counts);
-
-        for (var i = 0; i < timeKeys.length; i++) {
-            var key = timeKeys[i],
-                self = this,
-                newQuery = query;
+        for (let i = 0; i < timeKeys.length; i++) {
+            let newQuery = query;
 
             if(times[i] !== null){
                 newQuery += '&since=' + times[i];
@@ -173,31 +237,27 @@ class Stats extends React.Component {
 
             $.ajax({
                 url: '/api/stats/user?' + newQuery,
-                key: key,
+                key: timeKeys[i],
                 type: 'GET',
                 success: function(response, status, xhr) {
                     //Do nothing, because of bad response
-                    if(!response.data || response.err)
+                    if(!response.data || response.err){
                         $.snackbar({content: global.resolveError(response.err)});
-                    else {
-
-                        var counts = _.clone(self.state.counts);
-
+                    } else {
                         counts[this.key] = response.data.count;
 
-                        self.setState({ counts : counts } );
+                        self.setState({ counts });
                     }
                 },
                 error: (xhr, status, error) => {
-                    $.snackbar({content: global.resolveError(error)});
+                    return $.snackbar({ content: global.resolveError(error) });
                 }
             });
         }
     }
 
     render() {
-
-        var counts = this.state.counts;
+        const { counts } = this.state;
 
         return (
             <App user={this.props.user}>
@@ -209,6 +269,17 @@ class Stats extends React.Component {
                         <h3>Drag the polygon to calculate the number of users in an area or use the autocomplete.</h3>
 
                         <input type="text" ref="stats-autocomplete" placeholder="Enter a location" />
+
+                        <button 
+                            className="btn btn-flat pull-right mt12 mr16 kml" 
+                            onClick={this.kmlClicked}>Upload a KML file
+                        </button>
+
+                        <input 
+                            onChange={this.kmlFileInputChange} 
+                            type="file"
+                            accept="text/kml" 
+                            ref="kmlFileInput" />
 
                         <div id="stat-map" className="map"></div>
                     </div>
