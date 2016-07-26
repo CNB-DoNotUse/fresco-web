bfb8b43392f5534af8a98ee008fa172b10c7644b
Evan Rubinton <evanrubinton@gmail.com>
2016-07-26 12:26:54 -0400

    continue updating/refactoring gallery edit

--- app/platform/components/gallery/edit.js
+++ app/platform/components/gallery/edit.js
@@ -15,221 +15,108 @@ import _ from 'lodash';
  * Component for adding gallery editing to the current view
  */
 class Edit extends React.Component {
-
     constructor(props) {
         super(props);
-        const { gallery } = props;
 
-        this.state = {
-            gallery,
-            caption: gallery ? gallery.caption : '',
-            posts: gallery ? gallery.posts : null,
-            ratingChanged: false,
-            deletePosts: [],
-        };
+        this.state = this.getStateFromProps(props);
     }
 
     componentDidMount() {
         $.material.init();
     }
 
+    // If props has a gallery, and GalleryEdit does not currently have a
+    // gallery or the galleries are not the same
     componentWillReceiveProps(nextProps) {
-        // If props has a gallery, and GalleryEdit does not currently have a
-        // gallery or the galleries are not the same
-        if (nextProps.gallery &&
-            (!this.state.gallery || (this.state.gallery.id !== nextProps.gallery.id))) {
-            this.setState({
-                gallery: nextProps.gallery,
-                posts: nextProps.gallery.posts.map(p => p.id),
-            });
-            // TODO: is this necessary?
-            // $.material.init();
+        if (this.props.gallery.id !== nextProps.gallery.id) {
+            this.revert();
         }
     }
 
+	/**
+	 * Updates state map location when AutocompleteMap gives new location
+	 */
     onPlaceChange(place) {
-        const gallery = this.state.gallery;
-        gallery.location = place.location;
-        gallery.address = place.address;
-
-        this.setState({ gallery });
-    }
-
-    updateCaption(e) {
-        const gallery = this.state.gallery;
-        gallery.caption = e.target.value;
-
-        this.setState({ gallery });
-    }
-
-    updateGalleryField(key, value) {
-        const gallery = this.state.gallery;
-        gallery[key] = value;
-        this.setState({ gallery });
-    }
-
-    updateRelatedStories(stories) {
-        const gallery = this.state.gallery;
-        gallery.related_stories = stories;
-
-        this.setState({ gallery });
-    }
-
-    updateArticles(articles) {
-        const gallery = this.state.gallery;
-        gallery.articles = articles;
-
-        this.setState({ gallery });
-    }
-
-    updateTags(tags) {
-        const gallery = this.state.gallery;
-        gallery.tags = tags;
-
-        this.setState({ gallery });
-    }
-
-    toggleDeletePost(post) {
-        const index = this.state.deletePosts.indexOf(post);
-
-        if (index === -1) {
-            this.setState({ deletePosts: this.state.deletePosts.concat(post) });
-        } else {
-            const posts = this.state.deletePosts;
-            posts.splice(index, 1);
-            this.setState({ deletePosts: posts });
-        }
-    }
-
-    updateRating(rating) {
-        const { gallery } = this.state;
-        gallery.rating = rating;
-
-        // Update new gallery
-        this.setState({ gallery, ratingChanged: true });
-    }
-
-    toggleHighlight(e) {
-        this.updateRating(e.target.checked ? 3 : 2);
+        this.setState({ address: place.address, location: place.location });
     }
 
     /**
- 	 * Updates GalleryEdit Gallery
- 	 * Used by GalelryEditFoot's Add Files
- 	 */
-    updateGallery(gallery) {
-        this.setState({ gallery });
-    }
+     * getStateFromProps
+     *
+     * @param {object} props
+     * @returns {object} initial state from passed props
+     */
+    getStateFromProps(props) {
+        const { gallery } = props;
 
-    revertGallery() {
-        // Set gallery back to original
-        const { gallery } = this.props;
-        this.setState({
-            gallery: _.clone(gallery, true),
-            posts: gallery.posts.map(p => p.id),
-            deletePosts: [],
-        });
-    }
-
-    unverifyGallery() {
-        this.saveGallery(null, { rating: 1 });
-    }
-
-    verifyGallery() {
-        this.saveGallery(null, { rating: 2 });
-    }
-
-    saveGallery(event, passedParams) {
-        const { gallery } = this.state;
-        const files = gallery.files ? gallery.files : [];
-        const caption = gallery.caption;
-        const tags = gallery.tags;
-        let assignment = gallery.assignment ? gallery.assignment.id : undefined;
-
-
-        // If assignment was removed, send -1 instead of undefined.
-        if (this.props.gallery.assignment && this.props.gallery.assignment.id && !assignment) {
-            assignment = -1;
-        }
-
-        // Generate post ids for update
-        const posts = _.difference(this.state.posts, this.state.deletePosts);
-
-        if (posts.length + files.length === 0) {
-            $.snackbar({ content: 'Galleries must have at least 1 post' });
-            return;
-        }
-
-        // TODO: deprecated
-        // Generate stories for update
-        // var stories = gallery.related_stories
-        //     ? gallery.related_stories.map((story) => {
-        //         if (story.new) return 'NEW=' + JSON.stringify(story);
-        //         return story.id;
-        //     })
-        //     : null;
-
-        // TODO: deprecated
-        //Generate articles for update
-        // var articles = gallery.articles.map((article) => {
-        //     if(article.new) return 'NEW=' + JSON.stringify(article);
-        //     return article.id;
-        // });
-
-        //Configure params for the updated gallery
-        let params = {
-            caption,
-            // posts: posts,
-            // assignment: assignment,
-            tags,
-            // stories: stories,
-            // articles: articles,
+        return {
+            tags: gallery.tags || [],
+            stories: gallery.stories,
+            assignment: gallery.assignment,
+            address: gallery.address,
+            loading: false,
+            caption: gallery.caption || 'No Caption',
+            postIds: gallery.posts.map((p) => p.id),
+            postIdsToDelete: [],
+            articles: gallery.articles,
+            rating: gallery.rating,
         };
+    }
 
-        if(typeof(passedParams) == 'object') {
-            params = _.extend(params, passedParams);
+    /**
+     * getFormData
+     *
+     * @returns {Object} Form Data Object
+     */
+    getFormData() {
+        const {
+            tags,
+            caption,
+            address,
+            location,
+            stories,
+            assignment,
+            gallery,
+            postIdsToDelete,
+            postIds,
+        } = this.state;
+        const posts = _.difference(postIds, postIdsToDelete);
+        const files = gallery.files ? gallery.files : [];
+
+        if (caption.length === 0) {
+            $.snackbar({ content: 'A gallery must have a caption' });
+            return null;
+        }
+
+        if (!posts.length || !files.length) {
+            $.snackbar({ content: 'Galleries must have at least 1 post' });
+            return null;
         }
 
         // Configure the byline's other origin
         // var byline = utils.getBylineFromComponent(gallery, this.refs.galleryEditBody.refs.byline);
         // _.extend(params, byline);
 
- 		// Check if imported and there is a location to add a location to the save request
-        if (gallery.imported && gallery.location) {
-            // if (gallery.location.lat && gallery.location.lng) {
-            //     params.lat = gallery.location.lat;
-            //     params.lon = gallery.location.lng;
-            // } else {
-            //     const { lat, lng } = utils.getCentroid(
-            //         gallery.location.coordinates[0].map((coord) => {
-            //             return {
-            //                 lat: coord[1],
-            //                 lng: coord[0]
-            //             }
-            //         }));
+        const params = {
+            tags,
+            caption,
+            address,
+            geo: utils.getGeoFromCoord(location),
+            stories,
+            posts,
+            assignment_id: assignment ? assignment.id : null,
+        };
 
-            //     params.lat = lat;
-            //     params.lng = lng;
-            // }
-            if (gallery.address) {
-                params.address = gallery.address;
-            }
-        }
-
-        // TODO: figure out file uploading
-        // if (files.length) {
-        //     // Upload files then update gallery in callback
-        //     this.uploadNewFiles(gallery, files, (newPosts) => {
-        //         this.requestUpdateGallery(newPosts);
-        //     });
-        // } else {
-            // Or just update gallery if no files present
-        this.requestUpdateGallery(params);
-        // }
+        return params;
     }
 
-    requestUpdateGallery(params) {
-        const { gallery } = this.props;
+    // TODO: figure out file uploading
+    save(rating) {
+        const params = this.getFormData();
+        const { gallery, onUpdateGallery } = this.props;
+        if (!gallery.id || !params) return;
+        params.rating = rating;
+
         $.ajax(`/api/gallery/${gallery.id}/update`, {
             method: 'post',
             contentType: 'application/json',
@@ -237,7 +124,7 @@ class Edit extends React.Component {
         })
         .done((response) => {
             // Update parent gallery
-            this.props.onUpdateGallery(response.body);
+            onUpdateGallery(response.body);
             // Hide the modal
             this.hide();
         })
@@ -248,38 +135,39 @@ class Edit extends React.Component {
         });
     }
 
-    // uploadNewFiles(gallery, files, callback) {
-    //     var data = new FormData();
+    /**
+	 * Reverts all changes
+	 */
+    revert() {
+        if (this.state.loading) return;
 
-    //     for (var i = 0; i < files.length; i++) {
-    //         data.append(i, files[i]);
-    //     }
+        this.setState(this.getStateFromProps(this.props));
+    }
 
-    //     data.append('gallery', gallery.id);
+    // TODO: refactor
+    toggleDeletePost(post) {
+        const index = this.state.postIdsToDelete.indexOf(post);
 
-    //     $.ajax({
-    //         url: '/api/gallery/addpost',
-    //         type: 'POST',
-    //         data,
-    //         processData: false,
-    //         contentType: false,
-    //         cache: false,
-    //         dataType: 'json',
-    //         success: (result, status, xhr) => {
-    //             callback(result.data);
-    //         },
-    //         error: (xhr, status, error) => {
-    //             $.snackbar({content: utils.resolveError(error)});
-    //         },
-    //         xhr: () => {
-    //             const xhr = $.ajaxSettings.xhr();
-    //             xhr.upload.onprogress = function () {};
-    //             xhr.upload.onload = function () {};
+        if (index === -1) {
+            this.setState({ postIdsToDelete: this.state.postIdsToDelete.concat(post) });
+        } else {
+            const posts = this.state.postIdsToDelete;
+            posts.splice(index, 1);
+            this.setState({ postIdsToDelete: posts });
+        }
+    }
 
-    //             return xhr;
-    //         },
-    //     });
-    // }
+    toggleHighlight(e) {
+        this.setState({ rating: e.target.checked ? 3 : 2 });
+    }
+
+    /**
+ 	 * Updates GalleryEdit Gallery
+ 	 * Used by GalelryEditFoot's Add Files
+ 	 */
+    updateGallery(gallery) {
+        this.setState({ gallery });
+    }
 
     hide() {
         this.setState({ gallery: null });
@@ -287,8 +175,16 @@ class Edit extends React.Component {
     }
 
     renderBody() {
-        const { gallery, deletePosts } = this.state;
-        const { user } = this.props;
+        const {
+            postIdsToDelete,
+            stories,
+            caption,
+            assignment,
+            tags,
+            rating,
+            articles,
+        } = this.state;
+        const { user, gallery } = this.props;
 
         return (
             <div className="col-xs-12 col-lg-12 edit-new dialog">
@@ -310,30 +206,30 @@ class Edit extends React.Component {
                                 type="text"
                                 className="form-control floating-label"
                                 ref="gallery-caption"
-                                value={gallery.caption}
+                                value={caption}
                                 placeholder="Caption"
-                                onChange={(e) => this.updateCaption(e)}
+                                onChange={(e) => this.setState({ caption: e.target.value })}
                             />
                         </div>
 
                         <EditAssignment
-                            assignment={gallery.assignment}
-                            updateAssignment={(a) => this.updateAssignment(a)}
+                            assignment={assignment}
+                            updateAssignment={(a) => this.setState({ assignment: a })}
                         />
 
                         <EditTags
-                            tags={gallery.tags}
-                            updateTags={(t) => this.updateTags(t)}
+                            tags={tags}
+                            updateTags={(t) => this.setState({ tags: t })}
                         />
 
                         <EditStories
-                            relatedStories={gallery.related_stories}
-                            updateRelatedStories={(s) => this.updateRelatedStories(s)}
+                            relatedStories={stories}
+                            updateRelatedStories={(s) => this.setState({ stories: s })}
                         />
 
                         <EditArticles
-                            articles={gallery.articles}
-                            updateArticles={(a) => this.updateArticles(a)}
+                            articles={articles}
+                            updateArticles={(a) => this.updateArticles({ articles: a })}
                         />
 
                         <div className="dialog-row">
@@ -341,7 +237,7 @@ class Edit extends React.Component {
                                 <label>
                                     <input
                                         type="checkbox"
-                                        checked={gallery.rating == 3}
+                                        checked={rating === 3}
                                         onChange={(e) => this.toggleHighlight(e)}
                                     />
                                     Highlighted
@@ -353,23 +249,23 @@ class Edit extends React.Component {
                     <EditPosts
                         posts={gallery.posts}
                         files={gallery.files}
-                        deletePosts={deletePosts}
+                        deletePosts={postIdsToDelete}
                         toggleDelete={(p) => this.toggleDeletePost(p)}
                     />
 
                     <EditMap
                         gallery={gallery}
                         onPlaceChange={(p) => this.onPlaceChange(p)}
-                        disabled={user.id === gallery.owner_id}
+                        disabled={user.id !== gallery.owner_id}
                     />
                 </div>
 
                 <EditFoot
-                    gallery={this.state.gallery}
-                    revert={() => this.revertGallery()}
-                    saveGallery={(e, p) => this.saveGallery(e, p)}
-                    verifyGallery={() => this.verifyGallery()}
-                    unverifyGallery={() => this.unverifyGallery()}
+                    gallery={gallery}
+                    revert={() => this.revert()}
+                    saveGallery={(r) => this.save(r)}
+                    verifyGallery={() => this.save(2)}
+                    unverifyGallery={() => this.unverifyGallery(1)}
                     updateGallery={(g) => this.updateGallery(g)}
                     hide={() => this.hide()}
                 />
@@ -378,8 +274,7 @@ class Edit extends React.Component {
     }
 
     render() {
-        const { gallery } = this.state;
-        const { toggled } = this.props;
+        const { toggled, gallery } = this.props;
 
         return (
             <div>
@@ -397,11 +292,11 @@ Edit.propTypes = {
     toggled: PropTypes.bool,
     toggle: PropTypes.func,
     onUpdateGallery: PropTypes.func,
+    user: PropTypes.object,
 };
 
 Edit.defaultProps = {
     gallery: null,
-    posts: [],
     toggled: false,
     onUpdateGallery() {},
     toggle() {},
