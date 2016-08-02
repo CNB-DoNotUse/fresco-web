9893a28a624a2a9214b017740cca70687e177742
Evan Rubinton <evanrubinton@gmail.com>
2016-08-02 15:33:58 -0400

    delete deprecated webpack config, add fix for navbar tab-control style, add default outlet id of user's outlet to routes/outlet

--- routes/outlet.js
+++ routes/outlet.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const config = require('../lib/config');
-const resolveError = require('../lib/resolveError');
 const router = express.Router();
 const API = require('../lib/api');
 
@@ -8,27 +7,29 @@ const API = require('../lib/api');
  *  Outlet settings page for current logged in user
  */
 router.get('/settings', (req, res, next) => {
-    const { user, token } = req.session
+    const { user, token } = req.session;
 
     if (!user.outlet) {
-        return next({
+        next({
             message: 'No outlet found!',
-            status: 500
+            status: 500,
         });
+
+        return;
     }
 
-    //Chain promises for two API calls
+    // Chain promises for two API calls
     Promise.all([
         API.request({
             url: '/outlet',
             token,
-        })
+        }),
         // API.request({
         //     url: '/outlet/payment',
         //     token,
         // })
     ])
-    .then(responses => { 
+    .then(responses => {
         console.log(responses);
 
         const outlet = responses[0];
@@ -57,7 +58,7 @@ router.get('/settings', (req, res, next) => {
         next({
             message: 'Outlet not found!',
             status: error.status || 500,
-        })
+        });
     });
 });
 
@@ -66,17 +67,18 @@ router.get('/settings', (req, res, next) => {
  * @description
  */
 router.get('/:id?', (req, res, next) => {
-    const id = req.params.id || '';
+    const { user } = req.session;
+    const id = req.params.id
+        || (user.outlet ? user.outlet.id : '');
 
-    //Make request for full outlet object
+    // Make request for full outlet object
     API.request({
         method: 'GET',
         url: `/outlet/${id}`,
-        token: req.session.token
+        token: req.session.token,
     })
     .then((response) => {
         const outlet = response.body;
-        const { user } = req.session;
         const title = outlet.title;
         const props = JSON.stringify({ user, title, outlet });
 
@@ -87,13 +89,14 @@ router.get('/:id?', (req, res, next) => {
             page: 'outlet',
         });
     })
-    .catch((error) => {
-        return next({
+    .catch((error) => (
+        next({
             message: 'It seems like we couldn\'t locate your outlet!',
-            status: error.status || 500
-        });
-    });
+            status: error.status || 500,
+        })
+    ));
 });
 
 
 module.exports = router;
+
