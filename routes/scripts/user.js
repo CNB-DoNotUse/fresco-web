4f1ccef6e25e36798a856fbf528b099584a5661e
Evan Rubinton <evanrubinton@gmail.com>
2016-08-09 11:03:39 -0400

    remove unused imports in scripts/user

--- routes/scripts/user.js
+++ routes/scripts/user.js
@@ -1,35 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const validator = require('validator');
-const config = require('../../lib/config');
 const User = require('../../lib/user');
 const API = require('../../lib/api');
-const resolveError = require('../../lib/resolveError');
-const utils = require('../../lib/utils');
 
 /**
  * Processes login in for web platform users
  */
-router.post('/login', (req, res, next) => {
+router.post('/login', (req, res) => {
     API.request({
         method: 'POST',
         url: '/auth/signin',
         body: {
             username: req.body.email,
-            password: req.body.password
-        }
+            password: req.body.password,
+        },
     })
     .then(response => {
-        let { token, user } = response.body;
+        const { token, user } = response.body;
 
         User
-            .saveSession(req, user, token)
-            .then(() => {
-                return res.status(response.status).json({ success: true });
-            })
-            .catch((error) => {
-                return res.status(response.status).json({ success: false, error });
-            })
+        .saveSession(req, user, token)
+        .then(() => {
+            return res.status(response.status).json({ success: true });
+        })
+        .catch((error) => {
+            return res.status(response.status).json({ success: false, error });
+        })
     })
     .catch(error => API.handleError(error, res));
 });
@@ -42,32 +38,28 @@ router.get('/logout', User.logout);
 /**
  * Registers a new user account, optionally with an outlet
  */
-router.post('/register', (req, res, next) => {
+router.post('/register', (req, res) => {
     API.request({
         method: 'POST',
         url: '/auth/register',
-        body: req.body
+        body: req.body,
     })
     .then(response => {
         const { token } = response.body;
 
-        //Make request for full user object for session
+        // Make request for full user object for session
         API.request({
             method: 'GET',
             url: '/user/me',
-            token
+            token,
         })
         .then(response => {
             const user = response.body;
 
             User
-                .saveSession(req, user, token)
-                .then(() => {
-                    return res.status(response.status).json({ success: true });
-                })
-                .catch((error) => {
-                    return res.status(response.status).json({ success: false, error });
-                });
+            .saveSession(req, user, token)
+            .then(() => (res.status(response.status).json({ success: true })))
+            .catch((error) => (res.status(response.status).json({ success: false, error })));
         })
         .catch(error => API.handleError(error, res));
     })
