ca65e6a62e68dcfa41fa7aef5de1d0734e4e26c5
Evan Rubinton <evanrubinton@gmail.com>
2016-08-15 17:42:58 -0400

    use fresco image in gallery edit post(fixes style bugs in slider when empty image)

--- app/platform/components/gallery/edit-post.js
+++ app/platform/components/gallery/edit-post.js
@@ -1,37 +1,29 @@
 import React, { PropTypes } from 'react';
 import utils from 'utils';
+import FrescoImage from '../global/fresco-image';
 
 /**
  * Single Edit-Post Element
  * @description Post element that is wrapped inside container slick usually
  */
 
-class EditPost extends React.Component {
-    render() {
-        const { post } = this.props;
-
-        if (post.video) {
-            return (
-                <video width="100%" height="100%" data-id={post.id} controls>
-                    <source
-                        src={utils.formatVideo(post.video)}
-                        type="video/mp4"
-                    />
-                    Your browser does not support the video tag.
-                </video>
-            );
-        }
-
+const EditPost = ({ post }) => {
+    if (post.video) {
         return (
-            <img
-                role="presentation"
-                className="img-responsive"
-                src={utils.formatImg(post.image, 'medium')}
-                data-id={post.id}
-            />
+            <video width="100%" height="100%" data-id={post.id} controls>
+                <source
+                    src={utils.formatVideo(post.video)}
+                    type="video/mp4"
+                />
+                Your browser does not support the video tag.
+            </video>
         );
     }
-}
+
+    return (
+        <FrescoImage image={post.image} size="medium" />
+    );
+};
 
 EditPost.propTypes = {
     post: PropTypes.object,
--- app/platform/components/global/fresco-image.js
+++ app/platform/components/global/fresco-image.js
@@ -1,63 +1,61 @@
 import React, { PropTypes } from 'react';
-import utils from 'utils'
+import utils from 'utils';
 
 /**
  * Stateless image that manages an image error handler
  * @description Will set a timeout to keep loading the image if it 404s
  */
-export default class FrescoImage extends React.Component {
+class FrescoImage extends React.Component {
+    componentDidMount() {
+        const { img } = this.refs;
 
-	constructor(props) {
-		super(props);
-	}
+        img.onerror = () => {
+            const timeout = parseInt(img.getAttribute('data-t') || 1, 10);
+            const lastTimeout = parseInt(img.getAttribute('data-lt') || 1, 10);
+            const image = `${utils.CDN}/images/missing.png`;
 
-	componentDidMount() {
-		const { size } = this.props;
-		const { img } = this.refs;
+            img.setAttribute('data-lt', timeout);
+            img.setAttribute('data-t', timeout + lastTimeout);
+            img.setAttribute('data-src', img.getAttribute('src'));
+            img.setAttribute('src', image);
 
-		img.onerror = () => {
-		    const timeout = parseInt(img.getAttribute('data-t') || 1);
-		    const lastTimeout = parseInt(img.getAttribute('data-lt') || 1);
-		    const image = `${utils.CDN}/images/missing.png`;
+            setTimeout(() => {
+                img.setAttribute('src', img.getAttribute('data-src'));
+            }, timeout * 1000);
 
-		    img.setAttribute('data-lt', timeout);
-		    img.setAttribute('data-t', timeout + lastTimeout);
-		    img.setAttribute('data-src', img.getAttribute('src'));
-		    img.setAttribute('src', image);
+            this.props.updateImage(image);
+        };
+    }
 
-		    setTimeout(() => {
+    render() {
+        const { image, size } = this.props;
+        const src = utils.formatImg(image, size);
 
-		        img.setAttribute('src', img.getAttribute('data-src'));
-
-		    }, timeout * 1000);
-
-		    this.props.updateImage(image);
-		}
-	}
-
-	render() {
-		const { image, size } = this.props;
-		const src = utils.formatImg(image, size);
-
-		return (
-			<div className="img">
-				<img
-					className={this.props.imageClass || 'img-cover'}
-					ref='img'
-					data-src={src}
-					src={src} />
-			</div>
-		)
-	}
+        return (
+            <div className="img">
+                <img
+                    className={this.props.imageClass || 'img-cover'}
+                    role="presentation"
+                    ref="img"
+                    data-src={src}
+                    src={src}
+                />
+            </div>
+        );
+    }
 }
 
 FrescoImage.propTypes = {
     size: PropTypes.string,
     image: PropTypes.string,
-    updateImage: PropTypes.func
+    updateImage: PropTypes.func,
+    imageClass: PropTypes.string,
 };
 
 FrescoImage.defaultProps = {
-	size: 'small',
-	updateImage: ()=>{}
-}
+    size: 'small',
+    updateImage: () => {},
+};
+
+export default FrescoImage;
+
