c87d365e11b97bf6affdf3dce420153d7596a878
Evan Rubinton <evanrubinton@gmail.com>
2016-08-17 13:47:01 -0400

    remove unused/deprecated funcstions in post detail

--- app/platform/views/postDetail.js
+++ app/platform/views/postDetail.js
@@ -7,7 +7,6 @@ import PostRelated from './../components/post/related';
 import PostRelatedTags from './../components/post/related-tags';
 import PostDetailImage from './../components/post/detail-image';
 import GalleryEdit from './../components/gallery/edit';
-import utils from 'utils';
 
 /**
  * Post Detail Parent Object, made of a side column and PostList
@@ -20,7 +19,6 @@ class PostDetail extends React.Component {
             galleryEditToggled: false,
             gallery: props.gallery,
             post: props.post,
-            loading: false,
         };
     }
 
@@ -32,60 +30,9 @@ class PostDetail extends React.Component {
         this.setState({ galleryEditToggled: !this.state.galleryEditToggled });
     }
 
-    saveGallery(id, params) {
-        if (!id || !params || this.state.loading) return;
-        this.setState({ loading: true });
-
-        $.ajax(`/api/gallery/${id}/update`, {
-            method: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(params),
-        })
-        .done((res) => {
-            // Update parent gallery
-            this.setState({ gallery: res });
-            // Hide the modal
-            this.toggleGalleryEdit();
-        })
-        .fail((err) => {
-            $.snackbar({
-                content: utils.resolveError(err, 'There was an error saving the gallery!'),
-            });
-        })
-        .always(() => {
-            this.setState({ loading: false });
-        });
-    }
-
-    removeGallery(id) {
-        if (!id || this.state.loading) return;
-
-        alertify.confirm('Are you sure you want to delete this gallery?', (confirmed) => {
-            if (!confirmed) return;
-            this.setState({ loading: true });
-
-            $.ajax({
-                url: `/api/gallery/${id}/delete`,
-                method: 'POST',
-                dataType: 'json',
-                contentType: 'application/json',
-            })
-            .done(() => {
-                $.snackbar({ content: 'Gallery deleted' });
-                location.href = document.referrer || '/highlights';
-            })
-            .fail(() => {
-                $.snackbar({ content: 'Unable to delete gallery' });
-            })
-            .always(() => {
-                this.setState({ loading: false });
-            });
-        });
-    }
-
     render() {
         const { user, title, verifier } = this.props;
-        const { gallery, galleryEditToggled, post, loading } = this.state;
+        const { gallery, galleryEditToggled, post } = this.state;
         let editable = (user.permissions.includes('update-other-content')) && !!gallery.id;
 
         return (
@@ -120,10 +67,6 @@ class PostDetail extends React.Component {
                 <GalleryEdit
                     gallery={gallery}
                     toggle={() => this.toggleGalleryEdit()}
-                    save={(id, p) => this.saveGallery(id, p)}
-                    remove={(id) => this.removeGallery(id)}
-                    loading={loading}
-                    user={user}
                     visible={galleryEditToggled && editable}
                     onUpdateGallery={(g) => this.onUpdateGallery(g)}
                 />
