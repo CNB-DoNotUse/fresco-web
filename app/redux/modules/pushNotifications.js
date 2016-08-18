865e99012992ba2bf302bce3db92b8c781a0dee0
Evan Rubinton <evanrubinton@gmail.com>
2016-08-18 16:20:28 -0400

    install redux, add redux folder with modules dir

---
+++ app/redux/modules/pushNotifications.js
@@ -0,0 +1,13 @@
+const SAVE = 'pushNotifications/SAVE';
+const SAVE_SUCCESS = 'pushNotifications/SAVE_SUCCESS';
+const SAVE_FAIL = 'pushNotifications/SAVE_FAIL';
+
+const initialState = {
+    loading: false,
+};
+
+export default function reducer(state = initialState, action = {}) {
+    switch (action.type) {
+            case SAVE
+    }
+}
---
+++ app/redux/modules/reducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import pushNotifications from './pushNotifications';
+
+export default combineReducers({
+  pushNotifications,
+});
+
