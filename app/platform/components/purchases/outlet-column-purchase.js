2549abd451a2cfbcd1f0de5ac2b9db9a0bba2208
Evan Rubinton <evanrubinton@gmail.com>
2016-09-02 12:32:11 -0400

    misc update/cleanup of purchases/outlet-column-purchase

--- app/platform/components/purchases/outlet-column-purchase.js
+++ app/platform/components/purchases/outlet-column-purchase.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import utils from 'utils';
 import moment from 'moment';
 
@@ -6,93 +6,90 @@ import moment from 'moment';
  * Purchase list item inside an outlet column
  */
 export default class OutletColumnPurchase extends React.Component {
-
-    constructor(props) {
-        super(props);
-
-        this.toggleVideo = this.toggleVideo.bind(this);
-        this.toggleVolume = this.toggleVolume.bind(this);
+    static propTypes = {
+        purchase: PropTypes.object,
     }
 
-    toggleVideo() {
-        var video = this.refs.video,
-            allVideos = document.getElementsByTagName('video');
+    toggleVideo = () => {
+        const video = this.video;
+        const allVideos = document.getElementsByTagName('video');
 
-        if(video.paused) {
-            for (var i = allVideos.length - 1; i >= 0; i--) {
-                if(!allVideos[i].paused)
-                    allVideos[i].pause();
-            }
-            video.play()
+        if (video.paused) {
+            allVideos.forEach(v => {
+                if (!v.paused) v.pause();
+            });
+            video.play();
         } else {
             video.pause();
         }
     }
 
-    toggleVolume() {
-        var video = this.refs.video;
+    toggleVolume = () => {
+        const video = this.video;
 
-        this.refs.volumeToggle.className = video.muted ? 'mdi mdi-volume-high volume-toggle' : 'mdi mdi-volume-off volume-toggle'
-        video.muted = video.muted ? false : true;
+        this.volumeToggle.className = video.muted
+            ? 'mdi mdi-volume-high volume-toggle'
+            : 'mdi mdi-volume-off volume-toggle';
+        video.muted = !video.muted;
     }
 
     render() {
-        var purchase = this.props.purchase,
-            post = this.props.purchase.post,
-            timestampText = '',
-            lastDay = Date.now() - 86400000,
-            media = '',
-            assignmentMeta = '';
+        const purchase = this.props.purchase;
+        const post = purchase.post;
+        const lastDay = Date.now() - 86400000;
+        let assignmentMeta = '';
+        let media = '';
+        let timestampText = '';
 
-        var name = post.owner ? post.owner.firstname + ' ' + post.owner.lastname : post.byline;
+        let name = post.owner
+            ? post.owner.full_name
+            : post.byline;
         name = name.replace('via Fresco News', '');
 
-        if(post.video !== null) {
-            var source = '';
+        if (post.video !== null) {
+            let source = '';
 
-            if(utils.isSafari(navigator.userAgent))
-                source = post.video
-            else
-                source = utils.formatVideo(post.video);
+            if (utils.isSafari(navigator.userAgent)) source = post.video;
+            else source = utils.formatVideo(post.video);
 
             media = [
-                        <span
-                            className="mdi mdi-volume-off volume-toggle"
-                            ref="volumeToggle"
-                            onClick={this.toggleVolume}
-                            key={0}>
-                        </span>,
-                        <video
-                            src={source}
-                            autoPlay={false}
-                            controls={false}
-                            loop={true}
-                            muted={true}
-                            onClick={this.toggleVideo}
-                            ref="video"
-                            key={1} >
-                        </video>
-                    ]
+                <span
+                    ref={r => this.volumeToggle = r}
+                    className="mdi mdi-volume-off volume-toggle"
+                    onClick={this.toggleVolume}
+                    key={0}
+                />,
+                <video
+                    ref={r => this.video = r}
+                    src={source}
+                    autoPlay={false}
+                    controls={false}
+                    onClick={this.toggleVideo}
+                    key={1}
+                    loop
+                    muted
+                />,
+            ];
         } else {
-
-            media = <img src={utils.formatImg(post.image, 'small')} />
-
+            media = <img src={utils.formatImg(post.image, 'small')} />;
         }
 
-        if(purchase.timestamp < lastDay) {
+        if (purchase.timestamp < lastDay) {
             timestampText = moment(purchase.timestamp).format('MMMM Do, YYYY')
         } else {
             timestampText = moment(purchase.timestamp).format('h:mm A') + ' UTC';
         }
 
-        if(purchase.assignment) {
-            assignmentMeta = <div className="meta-assignment">
-                                <a href={'/assignment/' + purchase.assignment._id}>
-                                    <span className="mdi mdi-logout-variant"></span>
+        if (purchase.assignment) {
+            assignmentMeta = (
+                <div className="meta-assignment">
+                    <a href={'/assignment/' + purchase.assignment._id}>
+                        <span className="mdi mdi-logout-variant" />
 
-                                    <span className="title">{purchase.assignment.title}</span>
-                                </a>
-                            </div>
+                        <span className="title">{purchase.assignment.title}</span>
+                    </a>
+                </div>
+            );
         }
 
         return (
@@ -111,7 +108,7 @@ export default class OutletColumnPurchase extends React.Component {
 
                 {assignmentMeta}
             </li>
-
-        )
+        );
     }
 }
+
