f92c3c46e619117edc888ab4e6f7096d9e96f826
Evan Rubinton <evanrubinton@gmail.com>
2016-09-12 16:48:51 -0400

    misc refactoring/updating of outlet-column cmp

--- app/platform/components/purchases/outlet-column.js
+++ app/platform/components/purchases/outlet-column.js
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react';
-import moment from 'moment';
-import utils from 'utils';
+import last from 'lodash/last';
+import api from 'app/lib/api';
 import OutletColumnHead from './outlet-column-head';
 import OutletColumnList from './outlet-column-list';
 
@@ -33,13 +33,10 @@ export default class OutletColumn extends React.Component {
 
     componentDidMount() {
         this.loadPurchaseStats();
-        this.loadGoal();
+        // this.loadGoal();
 
-        this.getPurchases(0, (purchases) => {
-            this.setState({
-                purchases,
-                offset: purchases.length,
-            });
+        this.loadPurchases(null, (purchases) => {
+            this.setState({ purchases });
         });
     }
 
@@ -51,138 +48,9 @@ export default class OutletColumn extends React.Component {
     }
 
     /**
-     * Requests purchases for a passed outlet
-     */
-    getPurchases(offset, callback) {
-        $.ajax({
-            url: '/api/outlet/purchases/list',
-            type: 'GET',
-            data: {
-                limit: 5,
-                offset,
-                sort: true,
-                details: true,
-                outlets: [this.state.outlet.id],
-            },
-            dataType: 'json',
-            success: response => callback(response.data),
-            error: (xhr, status, error) => {
-                $.snackbar({ content: utils.resolveError(error) });
-            },
-        });
-    }
-
-    /**
-     * Loads user stats for outlet and sets state
-     */
-    loadUserStats = () => {
-
-    }
-
-    /**
-     * Loads stats for purchases
-     */
-    loadPurchaseStats = () => {
-        const calculateStats = (stats) => {
-            let amount = 0;
-
-            if (typeof (stats.allTime) !== 'undefined') {
-                amount = stats.allTime.amount;
-            } else if (typeof (stats.since) !== 'undefined') {
-                amount = stats.since.amount;
-            }
-
-            if (amount === 0) return;
-
-            const stripeFee = (0.029 * amount) + 0.30;
-            const userFee = 0.67 * amount;
-            const margin = amount - stripeFee - userFee;
-
-            stats.margin = '$' + Math.round(margin * 100) / 100;
-            stats.revenue = '$' + amount;
-
-            this.setState({ purchaseStats: stats });
-        };
-
-        $.ajax({
-            url: '/api/outlet/purchases/stats',
-            type: 'GET',
-            data: {
-                outlets: [this.state.outlet.id],
-                since: this.props.since ? this.props.since.unix() * 1000 : null,
-                now: Date.now(),
-            },
-            dataType: 'json',
-            success: response => calculateStats(response),
-        });
-    }
-
-    loadGoal = () => {
-        $.ajax({
-            url: '/api/outlet/purchases/stats',
-            type: 'GET',
-            data: {
-                outlets: [this.state.outlet.id],
-                since: moment().utc().startOf('day').unix() * 1000,
-                now: Date.now(),
-            },
-            dataType: 'json',
-            success: response => this.setState({ dailyVideoCount: response.data.videos }),
-        });
-    }
-
-    /**
-     * Manages goal adjustment
-     * @param  {integer} increment Value to add to current goal
-     */
-    adjustGoal = (increment) => {
-        const outlet = this.state.outlet;
-        const params = { id: this.state.outlet.id };
-        const updateGoal = (data) => {
-            // Check if event set goal is still consistent with state goal
-            // before sending out request
-            if (data.goal !== this.state.outlet.goal) {
-                return;
-            }
-
-            $.ajax({
-                url: '/api/outlet/goal',
-                type: 'POST',
-                data,
-                dataType: 'json',
-                success: (response, status, xhr) => {
-                    // Set again based on response data for consistency
-                    // this.setState({
-                    //     outlet: response
-                    // });
-                },
-                error: (xhr, status, error) => {
-                    $.snackbar({
-                        content: utils.resolveError(error, 'There was an error updating this outlet\'s goal.'),
-                    });
-                },
-            });
-        };
-
-        if (typeof (outlet.goal) === 'undefined' || !outlet.goal) {
-            params.goal = 1;
-        } else {
-            params.goal = outlet.goal + increment;
-        }
-
-        // Set goal on outlet for state
-        outlet.goal = params.goal;
-        // Set for immediate feedback
-        this.setState({ outlet });
-
-        // Timeout in case of rapid clicks
-        setTimeout(() => updateGoal(params), 1000);
-    }
-
-    /**
      * Scroll listener for main window
      */
-    scroll = (e) => {
+    onScroll = (e) => {
         const grid = e.target;
         const scrollTop = grid.scrollTop;
         const head = this.columnHead.head;
@@ -194,18 +62,12 @@ export default class OutletColumn extends React.Component {
             head.className += ' shadow';
         }
 
+        const endOfScroll = grid.scrollTop > ((grid.scrollHeight - grid.offsetHeight) - 200);
+
         // Check that nothing is loading and that we're at the end of the scroll,
         // and that we have a parent bind to load  more posts
-        if (!this.state.loading &&
-            grid.scrollTop > ((grid.scrollHeight - grid.offsetHeight) - 200)) {
-            // Set that we're loading
-            this.setState({ loading: true });
-
-            // Run load on parent call
-            this.getPurchases(this.state.offset, (purchases) => {
-                this.loading = false;
-
-                // Disables scroll, and returns if posts are empty
+        if (!this.state.loading && endOfScroll) {
+            this.loadPurchases(last(this.state.purchases), (purchases) => {
                 if (!purchases || !purchases.length) {
                     this.setState({ scrollable: false });
                     return;
@@ -214,17 +76,143 @@ export default class OutletColumn extends React.Component {
                 // Set galleries from successful response, and unset loading
                 this.setState({
                     purchases: this.state.purchases.concat(purchases),
-                    offset: this.state.purchases.length + purchases.length,
                 });
             });
         }
     }
 
+    /**
+     * Requests purchases for a passed outlet
+     */
+    loadPurchases(lastPurchase = null, cb) {
+        const { outlet: { id } } = this.state;
+        if (!id) return;
+
+        this.setState({ loading: true });
+
+        api.get('purchase/list', {
+            outlet_ids: [id],
+            limit: 5,
+            last: lastPurchase,
+        })
+        .then(cb)
+        .catch(() => {
+            $.snackbar({
+                content: `There was an error getting outlet(${id}) purchases list`,
+            });
+        })
+        .then(() => {
+            // Set that we're loading
+            this.setState({ loading: false });
+        });
+    }
+
+    /**
+     * Loads user stats for outlet and sets state
+     */
+    // loadUserStats = () => {
+
+    // }
+
+    /**
+     * Loads stats for purchases
+     */
+    loadPurchaseStats = (lastPurchase = null) => {
+        const calculateStats = ({ total_revenue = 0 }) => {
+            if (!total_revenue) return;
+
+            const stripeFee = (0.029 * total_revenue) + 0.30;
+            const userFee = 0.67 * total_revenue;
+            const margin = total_revenue - stripeFee - userFee;
+
+            this.setState({ purchaseStats: {
+                margin: `$${Math.round(margin * 100) / 100}`,
+                revenue: `$${total_revenue}`,
+            } });
+        };
+
+        const { outlet: { id } } = this.state;
+
+        api.get('purchase/stats', {
+            outlet_ids: [id],
+            limit: 5,
+            last: lastPurchase,
+        })
+        .then(calculateStats)
+        .catch(() => {
+            $.snackbar({
+                content: `There was an error getting outlet(${id}) purchase stats`,
+            });
+        });
+    }
+
+    // loadGoal = () => {
+    //     $.ajax({
+    //         url: '/api/outlet/purchases/stats',
+    //         type: 'GET',
+    //         data: {
+    //             outlets: [this.state.outlet.id],
+    //             since: moment().utc().startOf('day').unix() * 1000,
+    //             now: Date.now(),
+    //         },
+    //         dataType: 'json',
+    //         success: response => this.setState({ dailyVideoCount: response.data.videos }),
+    //     });
+    // }
+
+    /**
+     * Manages goal adjustment
+     * @param  {integer} increment Value to add to current goal
+     */
+    // adjustGoal = (increment) => {
+    //     const outlet = this.state.outlet;
+    //     const params = { id: this.state.outlet.id };
+    //     const updateGoal = (data) => {
+    //         // Check if event set goal is still consistent with state goal
+    //         // before sending out request
+    //         if (data.goal !== this.state.outlet.goal) {
+    //             return;
+    //         }
+
+    //         $.ajax({
+    //             url: '/api/outlet/goal',
+    //             type: 'POST',
+    //             data,
+    //             dataType: 'json',
+    //             success: (response, status, xhr) => {
+    //                 // Set again based on response data for consistency
+    //                 // this.setState({
+    //                 //     outlet: response
+    //                 // });
+    //             },
+    //             error: (xhr, status, error) => {
+    //                 $.snackbar({
+    //                     content: utils.resolveError(error, 'There was an error updating this outlet\'s goal.'),
+    //                 });
+    //             },
+    //         });
+    //     };
+
+    //     if (typeof (outlet.goal) === 'undefined' || !outlet.goal) {
+    //         params.goal = 1;
+    //     } else {
+    //         params.goal = outlet.goal + increment;
+    //     }
+
+    //     // Set goal on outlet for state
+    //     outlet.goal = params.goal;
+    //     // Set for immediate feedback
+    //     this.setState({ outlet });
+
+    //     // Timeout in case of rapid clicks
+    //     setTimeout(() => updateGoal(params), 1000);
+    // }
+
     render() {
         return (
             <div className="outlet-column" draggable>
                 <OutletColumnHead
-                    ref={r => this.columnHead = r}
+                    ref={r => { this.columnHead = r; }}
                     adjustGoal={this.adjustGoal}
                     userStats={this.state.userStats}
                     purchaseStats={this.state.purchaseStats}
@@ -233,7 +221,7 @@ export default class OutletColumn extends React.Component {
                 />
 
                 <OutletColumnList
-                    scroll={this.scroll}
+                    onScroll={this.onScroll}
                     purchases={this.state.purchases}
                 />
             </div>
