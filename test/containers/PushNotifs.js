135fa0e47d619ab41e5c007cc50eb15d012af4a9
Evan Rubinton <evanrubinton@gmail.com>
2016-09-21 13:09:32 -0400

    update test/containers/PushNotifs with more tests

--- test/containers/PushNotifs.js
+++ test/containers/PushNotifs.js
@@ -7,6 +7,8 @@ import PushNotifs from 'app/platform/containers/PushNotifs';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import DefaultTemplate from 'app/platform/components/pushNotifs/default-template';
 import GalleryListTemplate from 'app/platform/components/pushNotifs/gallery-list-template';
+import AssignmentTemplate from 'app/platform/components/pushNotifs/assignment-template';
+import RecommendTemplate from 'app/platform/components/pushNotifs/recommend-template';
 import TitleBody from 'app/platform/components/pushNotifs/title-body';
 import { RestrictByLocation, RestrictByUser } from 'app/platform/components/pushNotifs/restrict-by';
 import ChipInput from 'app/platform/components/global/chip-input';
@@ -47,12 +49,35 @@ describe('<PushNotifs />', () => {
         expect(component.find(ChipInput)).to.have.length(1);
     });
 
-    // describe('<DefaultTemplate />', () => {
-    // });
+    it('should render <AssignmentTemplate /> when set as active tab', () => {
+        store.dispatch(pushActions.setActiveTab('assignment'));
+        expect(component.find(AssignmentTemplate)).to.have.length(1);
+        expect(component.find(TitleBody)).to.have.length(1);
+        expect(component.find(RestrictByLocation)).to.have.length(1);
+        expect(component.find(RestrictByUser)).to.have.length(1);
+        expect(component.find(ChipInput)).to.have.length(1);
+    });
 
-    // describe('<GalleryListTemplate />', () => {
-        // console.log('component props', component.instance().props);
-        // console.log('component debug', component.debug());
-    // });
+    it('should render <RecommendTemplate /> when set as active tab', () => {
+        store.dispatch(pushActions.setActiveTab('recommend'));
+        expect(component.find(RecommendTemplate)).to.have.length(1);
+        expect(component.find(TitleBody)).to.have.length(1);
+        expect(component.find(RestrictByLocation)).to.have.length(1);
+        expect(component.find(RestrictByUser)).to.have.length(1);
+        expect(component.find(ChipInput)).to.have.length(2);
+    });
+
+    describe('<DefaultTemplate />', () => {
+        it('should propagate changes to inputs to redux store', () => {
+            component.find('input').first().simulate('change', { target: { value: 'test input' } });
+            component
+            .find('textarea').first()
+            .simulate('change', { target: { value: 'test textarea' } });
+
+            const templateState = store.getState().getIn(['pushNotifs', 'templates', 'default']);
+            expect(templateState.get('title')).to.equal('test input');
+            expect(templateState.get('body')).to.equal('test textarea');
+        });
+    });
 });
 
