29e388e15015fd038dfda410920c5612795d5b60
Evan Rubinton <evanrubinton@gmail.com>
2016-11-21 11:26:51 -0500

    update photos, videos, post list cmps with update storage api

--- app/platform/views/photos.js
+++ app/platform/views/photos.js
@@ -2,35 +2,28 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import utils from 'utils';
 import api from 'app/lib/api';
-import { createGetFromStorage, createSetInStorage } from 'app/lib/storage';
+import { getFromSessionStorage, setInSessionStorage } from 'app/lib/storage';
 import App from './app';
 import PostList from './../components/post/list';
 import TopBar from './../components/topbar';
-
-const getFromStorage = createGetFromStorage({ key: 'views/photos' });
-const setInStorage = createSetInStorage({ key: 'views/photos' });
 
 /**
  * Photos Parent Object (composed of PhotoList and Navbar)
  */
 class Photos extends React.Component {
-    static propTypes = {
-        sortBy: PropTypes.string,
-    };
-
     state = {
-        verifiedToggle: getFromStorage('verifiedToggle', true),
-        sortBy: getFromStorage('sortBy', 'created_at'),
+        verifiedToggle: getFromSessionStorage('topbar', 'verifiedToggle', true),
+        sortBy: getFromSessionStorage('topbar', 'sortBy', 'created_at'),
     };
 
     onVerifiedToggled = (toggled) => {
         this.setState({ verifiedToggle: toggled });
-        setInStorage({ verifiedToggle: toggled });
+        setInSessionStorage('topbar', { verifiedToggle: toggled });
     }
 
     onChronToggled = (sortBy) => {
         this.setState({ sortBy });
-        setInStorage({ sortBy });
+        setInSessionStorage('topbar', { sortBy });
     }
 
 	// Returns array of posts with last and callback, used in child PostList
