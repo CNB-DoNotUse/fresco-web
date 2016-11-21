4ba69876f34dd70a687b1de26a0c9e68177f01b2
Evan Rubinton <evanrubinton@gmail.com>
2016-11-21 11:20:42 -0500

    update lib/storage

--- app/lib/storage.js
+++ app/lib/storage.js
@@ -1,42 +1,42 @@
 // Session storage helper fns
 
-export const createGetFromStorage = ({ type = 'session', key }) => (dataKey, unfound = null) => {
-    if (type === 'session') {
-        if (sessionStorage.getItem(key)) {
-            const data = JSON.parse(sessionStorage.getItem(key));
-            return data[dataKey];
-        }
-    }
-
-    if (type === 'local') {
-        if (localStorage.getItem(key)) {
-            const data = JSON.parse(localStorage.getItem(key));
-            return data[dataKey];
-        }
+export const getFromSessionStorage = (key, dataKey, unfound = null) => {
+    if (sessionStorage.getItem(key)) {
+        const data = JSON.parse(sessionStorage.getItem(key));
+        return data[dataKey];
     }
 
     return unfound;
 };
 
-export const createSetInStorage = ({ type = 'session', key }) => (data) => {
-    let curData = {};
-
-    if (type === 'session') {
-        if (sessionStorage.getItem(key)) {
-            curData = JSON.parse(sessionStorage.getItem(key));
-        }
-
-        const newData = JSON.stringify(Object.assign(curData, data));
-        sessionStorage.setItem(key, newData);
+export const getFromLocalStorage = (key, dataKey, unfound = null) => {
+    if (localStorage.getItem(key)) {
+        const data = JSON.parse(localStorage.getItem(key));
+        return data[dataKey];
     }
 
-    if (type === 'local') {
-        if (localStorage.getItem(key)) {
-            curData = JSON.parse(localStorage.getItem(key));
-        }
-
-        const newData = JSON.stringify(Object.assign(curData, data));
-        localStorage.setItem(key, newData);
-    }
+    return unfound;
 };
 
+export const setInSessionStorage = (key, data) => {
+    let curData = {};
+
+    if (sessionStorage.getItem(key)) {
+        curData = JSON.parse(sessionStorage.getItem(key));
+    }
+
+    const newData = JSON.stringify(Object.assign(curData, data));
+    sessionStorage.setItem(key, newData);
+
+};
+
+export const setInLocalStorage = (key, data) => {
+    let curData = {};
+
+    if (localStorage.getItem(key)) {
+        curData = JSON.parse(localStorage.getItem(key));
+    }
+
+    const newData = JSON.stringify(Object.assign(curData, data));
+    localStorage.setItem(key, newData);
+};
