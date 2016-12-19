97b66511f15cbf4b250670e6faed78d97f07e4d9
Evan Rubinton <evanrubinton@gmail.com>
2016-12-19 12:41:02 -0500

    dont render 404 on fetch accepted users fail

--- routes/assignment.js
+++ routes/assignment.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const config = require('../lib/config');
-const router = express.Router();
 const api = require('../lib/api');
+
+const router = express.Router();
 
 router.get('/:id', (req, res, next) => {
     let user;
@@ -11,35 +12,45 @@ router.get('/:id', (req, res, next) => {
         user = req.session.user;
     }
 
-    Promise.all([
-        api.request({ token, url: `/assignment/${req.params.id}` }),
-        api.request({ token, url: `/assignment/${req.params.id}/accepted` }),
-    ]).then((apiRes) => {
-        const assignment = apiRes[0].body;
-        const acceptedUsers = apiRes[1].body;
-        const props = { user, assignment, acceptedUsers };
+    api.request({ token, url: `/assignment/${req.params.id}` })
+    .then((assignmentRes) => {
+        const assignment = assignmentRes.body;
+        const alerts = req.alerts || [];
+        let acceptedUsers = [];
 
-        if (user.permissions.includes('view-all-assignments')
-            || assignment.outlets.some(o => o.id === user.outlet.id)) {
-            res.render('app', {
-                props: JSON.stringify(props),
-                config,
-                alerts: req.alerts,
-                title: assignment.title,
-                page: 'assignmentDetail',
-            });
-        } else {
-            next({
-                message: 'Not authorized to view assignment.',
-                status: 401,
-            });
-        }
-    }).catch(() => (
+        // fetch accepted users for given assignment id
+        api.request({ token, url: `/assignment/${req.params.id}/accepted` })
+        .then((acceptedUsersRes) => {
+            acceptedUsers = acceptedUsersRes.body;
+        })
+        .catch(() => {
+            alerts.push('Unable to fetch accepted users');
+        })
+        .then(() => {
+            const props = { user, assignment, acceptedUsers };
+            if (user.permissions.includes('view-all-assignments')
+                || assignment.outlets.some(o => o.id === user.outlet.id)) {
+                res.render('app', {
+                    props: JSON.stringify(props),
+                    title: assignment.title,
+                    page: 'assignmentDetail',
+                    config,
+                    alerts,
+                });
+            } else {
+                next({
+                    message: 'Not authorized to view assignment.',
+                    status: 401,
+                });
+            }
+        });
+    }).catch(() => {
         next({
             message: 'Assignment not found!',
             status: 404,
-        })
-    ));
+        });
+    });
 });
 
 module.exports = router;
+
