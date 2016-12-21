2a2ddfd2f2c447a530680c85a8c92b8f15956a29
Evan Rubinton <evanrubinton@gmail.com>
2016-12-20 19:30:40 -0500

    continue building out googleMap cmp based on fullstackreact's tutorial

--- app/platform/components/googleMap/index.js
+++ app/platform/components/googleMap/index.js
@@ -3,33 +3,90 @@
 
 import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-import 'app/sass/platform/_publicGallery.scss';
+import camelCase from 'lodash/camelCase';
+
+const createHandlerName = evtName => camelCase(`on${evtName.toUpperCase()}`);
 
 class Map extends React.Component {
     static propTypes = {
-        lat: PropTypes.number,
-        lng: PropTypes.number,
+        initialLocation: PropTypes.shape({
+            lat: PropTypes.number,
+            lng: PropTypes.number,
+        }),
         zoom: PropTypes.number,
     }
 
     static defaultProps = {
-        lat: 37.774929,
-        lng: -122.419416,
+        initialLocation: { lng: -74, lat: 40.7 },
         zoom: 14,
+    }
+
+    static evtNames = ['click', 'dragend']
+
+    constructor(props) {
+        super(props);
+
+        Map.evtNames.forEach((e) => { Map.propTypes[createHandlerName(e)] = PropTypes.func; });
+    }
+
+    state = {
+        currentLocation: this.props.initialLocation,
     }
 
     componentDidMount() {
         this.loadMap();
     }
 
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.currentLocation !== this.state.currentLocation) {
+            this.recenterMap();
+        }
+    }
+
     loadMap() {
         if (google && google.maps) {
-            const { lat, lng, zoom } = this.props;
+            const { zoom } = this.props;
+            const { currentLocation: { lat, lng } } = this.state;
+
             const maps = google.maps;
             const node = ReactDOM.findDOMNode(this.mapCtr);
-            const center = new maps.LatLng(lat, lng);
+            const center = new maps.LatLng({ lat, lng });
             const mapConfig = Object.assign({}, { center, zoom });
+
             this.map = new maps.Map(node, mapConfig);
+
+            Map.evtNames.forEach(e => {
+                this.map.addListener(e, this.handleEvent(e));
+            });
+        }
+    }
+
+    handleEvent(evtName) {
+        let timeout;
+        const handlerName = createHandlerName(evtName);
+
+        return (e) => {
+            if (timeout) {
+                clearTimeout(timeout);
+                timeout = null;
+            }
+            timeout = setTimeout(() => {
+                if (this.props[handlerName]) {
+                    this.props[handlerName](this.props, this.map, e);
+                }
+            }, 0);
+        };
+    }
+
+    recenterMap() {
+        const map = this.map;
+        const { lat, lng } = this.state.currentLocation;
+
+        const maps = google.maps;
+
+        if (map) {
+            const center = new maps.LatLng(lat, lng);
+            map.panTo(center);
         }
     }
 
