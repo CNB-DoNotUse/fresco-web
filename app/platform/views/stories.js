45f922b679bc2f741b955fb2b74274ca3f3ec09f
Evan Rubinton <evanrubinton@gmail.com>
2016-12-30 11:33:15 -0500

    add location filter to story list, waiting on api fix

--- app/platform/views/stories.js
+++ app/platform/views/stories.js
@@ -1,64 +1,86 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
-import TopBar from './../components/topbar'
-import StoryList from './../components/global/story-list'
-import utils from 'utils'
+import React, { PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import TopBar from '../components/topbar';
+import StoryList from '../components/global/story-list';
+import LocationDropdown from '../components/topbar/location-dropdown';
+import { getFromSessionStorage, setInSessionStorage } from 'app/lib/storage';
+import { geoParams } from 'app/lib/location';
+import utils from 'utils';
 
 /**
  * Stories Parent Object, contains StoryList composed of StoryCells
  */
 class Stories extends React.Component {
+    static propTypes = {
+        user: PropTypes.object,
+    }
 
-	constructor(props) {
-		super(props);
-		
-		this.loadStories = this.loadStories.bind(this);
-	}
+    state = {
+        location: getFromSessionStorage('archive', 'location', {}),
+        reloadStories: false,
+    }
 
-	render() {
-		return (
-			<App 
-				user={this.props.user}
-				page="stories"
-			>
-				<TopBar
-					title="Stories"
-					timeToggle={true}
-					tagToggle={true} />
+    /**
+     * Called on Location dropdown state changes
+     */
+    onChangeLocation = (location) => {
+        this.setState({ location, reloadStories: true });
+        setInSessionStorage('archive', { location });
+    }
 
-				<StoryList
-					loadStories={this.loadStories}
-					scrollable={true} />
-			</App>
-		);
-	}
-
-	/**
-	 * Returns array of posts with offset and callback, used in child PostList	 
-	 */
-	loadStories (last, callback) {
-		const params = {
+    /**
+     * Returns array of posts with offset and callback, used in child PostList
+     */
+    loadStories = (last, callback) => {
+        const params = {
             last,
-			limit: 20,
-			sortBy: 'updated_at'
-		};
+            limit: 20,
+            sortBy: 'updated_at',
+            ...geoParams(this.state.location),
+        };
 
-		$.ajax({
-			url:  '/api/story/recent',
-			type: 'GET',
-			data: params,
-			dataType: 'json',
-			success: (stories) => callback(stories),
-			error: (xhr, status, error) => {
-				$.snackbar({ content:  'Couldn\'t fetch any stories!' });
-			}
-		});
-	}
+        $.ajax({
+            url: '/api/story/recent',
+            type: 'GET',
+            data: params,
+            dataType: 'json',
+            success: (stories) => {
+                this.setState({ reloadStories: false }, () => callback(stories));
+            },
+            error: () => {
+                $.snackbar({ content: 'Couldn\'t fetch any stories!' });
+            },
+        });
+    }
 
+    render() {
+        return (
+            <App user={this.props.user} page="stories">
+                <TopBar
+                    title="Stories"
+                    timeToggle
+                    tagToggle
+                >
+                    <LocationDropdown
+                        location={this.state.location}
+                        units="Miles"
+                        key="locationDropdown"
+                        onChangeLocation={this.onChangeLocation}
+                    />
+                </TopBar>
+
+                <StoryList
+                    loadStories={this.loadStories}
+                    reloadStories={this.state.reloadStories}
+                    scrollable
+                />
+            </App>
+        );
+    }
 }
 
 ReactDOM.render(
-  <Stories user={window.__initialProps__.user} />,
-  document.getElementById('app')
+    <Stories user={window.__initialProps__.user} />,
+    document.getElementById('app')
 );
