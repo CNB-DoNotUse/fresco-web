4193205a171eb7a89b63c66cdff0c3dc78edff23
Evan Rubinton <evanrubinton@gmail.com>
2017-01-02 13:00:31 -0500

    use new gallery/list cmp in views/galleries

--- app/platform/views/galleries.js
+++ app/platform/views/galleries.js
@@ -2,10 +2,13 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import { getFromSessionStorage, setInSessionStorage } from 'app/lib/storage';
 import App from './app';
-import GalleryList from '../components/global/gallery-list';
+import List from '../components/gallery/list';
 import TopBar from '../components/topbar';
 import TagFilter from '../components/topbar/tag-filter';
 import LocationDropdown from '../components/topbar/location-dropdown';
+import { geoParams } from 'app/lib/location';
+import isEqual from 'lodash/isEqual';
+import api from 'app/lib/api';
 
 /**
  * Galleries Parent Cmp (composed of GalleryList and Navbar)
@@ -19,7 +22,73 @@ class Galleries extends React.Component {
         verifiedToggle: getFromSessionStorage('topbar', 'verifiedToggle', true),
         location: getFromSessionStorage('archive', 'location', {}),
         tags: getFromSessionStorage('archive', 'tags', []),
+        galleries: [],
+        loading: false,
     };
+
+    componentDidMount() {
+        this.loadInitialGalleries();
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        const { tags, location, verifiedToggle } = this.state;
+        const newLocation = !isEqual(prevState.location, location);
+        const newTags = !isEqual(prevState.tags, tags);
+        const verifiedChanged = prevState.verifiedToggle !== verifiedToggle;
+        if (newLocation || newTags || verifiedChanged) this.loadInitialGalleries();
+    }
+
+    // Scroll listener for main window
+    onScroll = (e) => {
+        const grid = e.target;
+        const bottomReached = grid.scrollTop > ((grid.scrollHeight - grid.offsetHeight) - 400);
+
+        if (!this.state.loading && bottomReached){
+            const lastGallery = this.state.galleries[this.state.galleries.length - 1];
+            if (!lastGallery) return;
+            this.setState({ loading: true });
+
+            this.loadGalleries(lastGallery.id, (galleries) => {
+                if (!galleries) return;
+
+                // Set galleries from successful response
+                this.setState({
+                    galleries: this.state.galleries.concat(galleries),
+                    loading: false,
+                });
+            });
+        }
+    }
+
+    loadInitialGalleries = () => {
+        this.setState({ galleries: [] }, () => {
+            this.loadGalleries(null, (galleries) => {
+                this.list.grid.scrollTop = 0;
+                this.setState({ galleries });
+            });
+        });
+    }
+
+    // Returns array of galleries with offset and callback
+    loadGalleries = (last, callback) => {
+        const { tags, verifiedToggle, location } = this.state;
+        const params = {
+            limit: 20,
+            last,
+            tags,
+            ...geoParams(location),
+        };
+
+        if (verifiedToggle) params.rating = 2;
+        else params.rating = [0, 1, 2];
+
+        api
+        .get('gallery/list', params)
+        .then(callback)
+        .catch(() => {
+            $.snackbar({ content: 'Failed to load galleries' });
+        });
+    }
 
     onVerifiedToggled = (verifiedToggle) => {
         this.setState({ verifiedToggle });
@@ -30,7 +99,7 @@ class Galleries extends React.Component {
      * Called on Location dropdown state changes
      */
     onChangeLocation = (location) => {
-        this.setState({ location});
+        this.setState({ location });
         setInSessionStorage('archive', { location });
     }
 
@@ -47,7 +116,7 @@ class Galleries extends React.Component {
     }
 
     render() {
-        const { verifiedToggle, location, tags } = this.state;
+        const { verifiedToggle, location, tags, galleries } = this.state;
         const { user } = this.props;
 
         return (
@@ -75,12 +144,11 @@ class Galleries extends React.Component {
                     />
                 </TopBar>
 
-                <GalleryList
+                <List
+                    ref={(r) => { this.list = r; }}
+                    galleries={galleries}
                     withList={false}
-                    highlighted={false}
-                    onlyVerified={verifiedToggle}
-                    location={location}
-                    tags={tags}
+                    onScroll={this.onScroll}
                 />
             </App>
         );
