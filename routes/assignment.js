3f4f047804c7f56c87ff525fe70f1663f04a547a
Elmir Kouliev <elmir@kouliev.com>
2017-03-23 14:16:56 -0400

    Remove outlet check on assignment route

--- routes/assignment.js
+++ routes/assignment.js
@@ -5,68 +5,58 @@ const API = require('../lib/api');
 const router = express.Router();
 
 const fetchAcceptedUsers = ({ roles, assignmentId, token }) => {
-    //Only market managers should see this data
+    //Only admins should see this data
     if (!roles.includes('admin')) {
         return Promise.resolve([]);
     }
 
     //Fetch accepted users for given assignment id
     return API
-            .request({ token, url: `/assignment/${assignmentId}/accepted` })
-            .then((acceptedUsersRes) => {
-                return acceptedUsersRes.body;
-            });
+        .request({ token, url: `/assignment/${assignmentId}/accepted` })
+        .then((acceptedUsersRes) => {
+            return acceptedUsersRes.body;
+        });
 };
 
 router.get('/:id', (req, res, next) => {
     let user = req.session.user;
     let token = req.session.token.token;
 
-
     API
-    .request({ token, url: `/assignment/${req.params.id}` })
-    .then((assignmentRes) => {
-        const assignment = assignmentRes.body;
-        const alerts = req.alerts || [];
-        let acceptedUsers = [];
+        .request({ token, url: `/assignment/${req.params.id}` })
+        .then((assignmentRes) => {
+            const assignment = assignmentRes.body;
+            const alerts = req.alerts || [];
+            let acceptedUsers = [];
 
-
-        if (!(user.roles.includes('admin')
-            || assignment.outlets.some(o => o.id === user.outlet.id))) {
-            return next({
-                message: 'Not authorized to view assignment.',
-                status: 401,
+            fetchAcceptedUsers({
+                token,
+                roles: user.roles,
+                assignmentId: assignment.id
+            })
+            .then((acceptedRes) => {
+                acceptedUsers = acceptedRes;
+            })
+            .catch(() => {
+                alerts.push('Unable to fetch accepted users!');
+            })
+            .then(() => {
+                res.render('app', {
+                    props: JSON.stringify({ user, assignment, acceptedUsers }),
+                    title: assignment.title,
+                    page: 'assignmentDetail',
+                    config,
+                    alerts,
+                    referral: req.session.referral,
+                });
             });
-        }
-
-
-        fetchAcceptedUsers({
-            token,
-            roles: user.roles,
-            assignmentId: assignment.id
-        })
-        .then((acceptedRes) => {
-            acceptedUsers = acceptedRes;
         })
         .catch(() => {
-            alerts.push('Unable to fetch accepted users');
-        })
-        .then(() => {
-            res.render('app', {
-                props: JSON.stringify({ user, assignment, acceptedUsers }),
-                title: assignment.title,
-                page: 'assignmentDetail',
-                config,
-                alerts,
-                referral: req.session.referral,
+            next({
+                message: 'Assignment not found!',
+                status: 404,
             });
         });
-    }).catch(() => {
-        next({
-            message: 'Assignment not found!',
-            status: 404,
-        });
-    });
 });
 
 module.exports = router;
