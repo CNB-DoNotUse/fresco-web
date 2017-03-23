c0d27dd16b7f4695ed522a560f13d8ae920c565e
Elmir Kouliev <elmir@kouliev.com>
2017-03-23 18:46:56 -0400

    Doc updates

--- app.js
+++ app.js
@@ -124,7 +124,7 @@ app.use((req, res, next) => {
     const route = req.path.slice(1).split('/')[0];
     const now = Date.now();
 
-    // Check if not a platform route, then send onwwards
+    // Check if not a platform route i.e. user is not needed, then send onwwards
     if (routes.platform.indexOf(route) === -1) {
         return next();
     }
@@ -137,15 +137,16 @@ app.use((req, res, next) => {
         });
     }
 
-    const tokenExpired = new Date(req.session.token.expires_at) < now; //Bearer token has expired
+    //Bearer token has expired
+    const tokenExpired = new Date(req.session.token.expires_at) < now;
 
-    // Check if the token hasn't expired
+    // Check if the token has expired
     if (tokenExpired) {
         //Bearer token has expired, so refresh the token
         return userMiddleware.refreshBearer(req, res, next);
     }
 
-    // Check if the session hasn't expired
+    // Check if the session has expired
     if (!req.session.user.TTL || req.session.user.TTL - now < 0) {
        //Session has expired, so refresh the user
        return userMiddleware.refresh(req, res, next);
