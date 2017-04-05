f587f8ab606f2693e164f0e9f99ab81dbf99ffa4
Runtao0 <ry2228@columbia.edu>
2017-04-05 15:18:52 -0400

    knit-pick, remove extra comma

--- app/platform/components/global/chip-input.js
+++ app/platform/components/global/chip-input.js
@@ -237,7 +237,7 @@ class ChipInput extends React.Component {
             items,
             queryAttr,
             multiple,
-            altAttr,
+            altAttr
         } = this.props;
 
         //Run checks to make sure we don't already have the item
