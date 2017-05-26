0a957b2bf7428b8b4b7d235d7ed1a13560e716d7
Runtao0 <ry2228@columbia.edu>
2017-05-26 12:10:27 -0400

    API calls for stories, and reducers work for search bar

--- app/redux/actions/stories.js
+++ app/redux/actions/stories.js
@@ -2,14 +2,21 @@ import api from 'app/lib/api';
 import utils from 'utils';
 import { enableLoading, toggleSnackbar, toggleModal } from './ui';
 import { findById } from 'app/lib/helpers';
+import { initialSearchParams } from 'app/platform/views/stories';
 
 export const RECEIVE_STORIES = 'RECEIVE_STORIES';
 export const RECEIVE_ONE_STORY = 'RECEIVE_ONE_STORY';
 export const RECEIVE_POSTS = 'RECEIVE_POSTS';
 export const RECEIVE_ERROR = 'RECEIVE_ERROR';
-// export const UPDATE_STORY = 'UPDATE_STORY';
-// export const UPDATE_POST = 'UPDATE_POST';
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+export const REMOVE_USERS = 'REMOVE_USERS';
+export const MOVE_USERS = 'MOVE_USERS';
+export const RECEIVE_ASSIGNMENTS = 'RECEIVE_ASSIGNMENTS';
+export const REMOVE_ASSIGNMENTS = 'REMOVE_ASSIGNMENTS';
+export const MOVE_ASSIGNMENTS = 'MOVE_ASSIGNMENTS';
+export const RECEIVE_TAGS = 'RECEIVE_TAGS';
 export const EDIT_NEW_STORY = 'EDIT_NEW_STORY';
+export const RESET_PARAMS = "RESET_PARAMS";
 // export const CREATE_STORY = 'CREATE_STORY';
 // export const DELETE_POST = 'DELETE_POST';
 // export const DELETE_STORY = 'DELETE_STORY';
@@ -48,6 +55,35 @@ export const receiveError = error => ({
     error,
 });
 
+export const receiveUsers = users => ({
+    type: RECEIVE_USERS,
+    ...users,
+});
+export const removeUser = userID => ({
+    type: REMOVE_USERS,
+    userID,
+});
+export const moveUser = userID => ({
+    type: MOVE_USERS,
+    userID,
+});
+export const receiveAssignments = assignments => ({
+    type: RECEIVE_ASSIGNMENTS,
+    ...assignments,
+});
+export const removeAssignment = assignmentID => ({
+    type: REMOVE_ASSIGNMENTS,
+    assignmentID,
+});
+export const moveAssignment = assignmentID => ({
+    type: MOVE_ASSIGNMENTS,
+    assignmentID,
+});
+export const receiveTags = tags => ({
+    type: RECEIVE_TAGS,
+    ...tags,
+});
+
 // export const updateStory = story => ({
 //     type: UPDATE_STORY,
 //     story,
@@ -63,26 +99,75 @@ export const editNewStory = story => ({
     story,
 });
 
+export const resetParams = () => ({
+    type: RESET_PARAMS,
+    params: initialSearchParams
+})
 // export const createStory = story => ({
 //     type: CREATE_STORY,
 //     error,
 // });
 
 // async actions
-export function createStory(story) {
-  return (dispatch) => {
-    return api.post('/story/create', story)
-      .then(oneStory => dispatch(receiveOneStory(oneStory)),
-      error => dispatch(receiveError(error))
-    );
-  };
+export function getUsers(query) {
+    return (dispatch) => {
+        return api.get('search', {
+                [`users[q]`]: query
+            }).then(users => dispatch(receiveUsers(users)))
+            .catch(error => dispatch(receiveError(error)));
+    }
+}
+export function getAssignments(query) {
+    return (dispatch) => {
+        return api.get('search', {
+                [`assignments[q]`]: query
+            }).then(assignments => dispatch(receiveAssignments(assignments)))
+            .catch(error => dispatch(receiveError(error)));
+    }
+}
+export function getTags(query) {
+    return (dispatch) => {
+        return api.get('search', {
+                [`tags[q]`]: query
+            }).then(tags => {
+                dispatch(receiveTags(tags))
+            })
+            .catch(error => dispatch(receiveError(error)));
+    }
 }
 
-export function deleteStory(story) {
-  return (dispatch) => {
-    return api.post('')
-      .then(oneStory => dispatch(receiveOneStory(oneStory)),
-      error => dispatch(receiveError(error))
-    );
-  };
+export function createStory(story) {
+    return (dispatch) => {
+        return api.post('story/create', story)
+          .then(oneStory => dispatch(receiveOneStory(oneStory)),
+          error => dispatch(receiveError(error))
+        );
+    };
+}
+
+export function deleteStory(story_id) {
+    return (dispatch) => {
+        return api.post(`story/${story_id}/delete`)
+          .then(oneStory => dispatch(receiveOneStory(oneStory)),
+          error => dispatch(receiveError(error))
+        );
+    };
+}
+
+export function updateStory(story) {
+    return (dispatch) => {
+        return api.post(`story/${story.id}/update`, story)
+            .then(oneStory => dispatch(receiveOneStory(oneStory)),
+            error => dispatch(receiveError(error))
+        );
+    };
+}
+
+export function getStories() {
+    return (dispatch) => {
+        return api.get(`story/recent`)
+            .then(stories => dispatch(receiveStories(stories)),
+            error => dispatch(receiveError(error))
+        )
+    }
 }
--- app/redux/modules/searchParams_reducer.js
+++ app/redux/modules/searchParams_reducer.js
@@ -9,18 +9,87 @@ import {
     CHANGE_SEARCH,
     REMOVE_STORY,
     RECEIVE_USER_STATUS,
+    RECEIVE_USERS,
+    MOVE_USERS,
+    RECEIVE_TAGS,
+    RECEIVE_ASSIGNMENTS,
+    MOVE_ASSIGNMENTS,
+    RESET_PARAMS
 } from 'app/redux/actions/stories';
+import { setInLocalStorage } from 'app/lib/storage';
+
+import { fromJS } from 'immutable';
 
 const searchParams = (state = {}, action) => {
+    const newState = state.toJS();
     switch (action.type) {
         case CHANGE_SEARCH:
-        const { param, value } = action.searchParam;
-            if (value || value === "") {
-                return state.set( param, value );
+            const { param, value } = action.searchParam;
+                if (value || value === "") {
+                    return state.set( param, value ).set('users', {}).set('assignments', {});
+                } else {
+                    const currentVal = state.get(param);
+                    return state.set(param, !currentVal);
+                }
+            break;
+        case RECEIVE_USERS:
+            if (action.users.results) {
+                state.set('users', {});
+                const users = {};
+                action.users.results.forEach((user) => {
+                    if (!state.getIn(['params', 'users', user.id])) {
+                        users[user.id] = user;
+                    }
+                });
+                return state.set('users', users);
             } else {
-                const currentVal = state.get(param);
-                return state.set(param, !currentVal);
+                state.set('users', {});
             }
+            break;
+        case MOVE_USERS:
+            newState.params.users[action.userID] = newState.users[action.userID];
+            delete newState.users[action.userID];
+            return fromJS(newState);
+            break;
+        case RECEIVE_ASSIGNMENTS:
+            if (action.assignments.results) {
+                state.set('assignments', {});
+                const assignments = {};
+                action.assignments.results.forEach((assignment) => {
+                    if (!state.getIn(['params', 'assignments', assignment.id])) {
+                        assignments[assignment.id] = assignment;
+                    }
+                })
+                return state.set('assignments', assignments);
+            } else {
+                state.set('assignments', {});
+            }
+            break;
+        case MOVE_ASSIGNMENTS:
+            newState.params.assignments[action.assignmentID] = newState.assignments[action.assignmentID];
+            delete newState.assignments[action.assignmentID];
+            return fromJS(newState);
+        case RESET_PARAMS:
+            setInLocalStorage('searchParams', action.params);
+            return fromJS(action.params);
+            break;
+        // case RECEIVE_TAGS:
+        //     if (action.tags.results) {
+        //         state.set('tags', {});
+        //         const tags = {};
+        //         action.tags.results.slice(0, 3).forEach((tag) => {
+        //             tags[tag.id] = tag;
+        //         });
+        //         return state.set('tags', tags)
+        //     } else {
+        //         state.set('tags', {});
+        //     }
+        //     break;
+        //technically receiving errors is not necessary here since
+        //receiveError is never dispatched here, just included if neede in the future
+        case RECEIVE_ERROR:
+            return state.set('errors', action.error);
+            break;
         default:
             return state;
     }
