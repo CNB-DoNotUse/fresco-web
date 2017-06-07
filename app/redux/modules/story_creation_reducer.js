7b1141323ee59701af0c4200dd7bb2aa5373caac
Runtao0 <ry2228@columbia.edu>
2017-06-07 13:11:32 -0400

    create story creation reducer

---
+++ app/redux/modules/story_creation_reducer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import merge from 'lodash/merge';
+
+import {
+    RECEIVE_STORIES,
+    RECEIVE_ONE_STORY,
+} from 'app/redux/actions/stories';
+
+const storyCreation = (state = {}, action) => {
+    switch (expression) {
+        case expression:
+
+            break;
+        default:
+
+    }
+}
+
+export default storyCreation;
